2eeb650f85d9ebd4cd6a6a42de93d6d2bfbecb75
pfkeb <>
2000-02-21 20:45:18 +0000

    Move libfacilities shared library to top level

---
+++ facilities/version.cxx
@@ -0,0 +1,12 @@
+/*
+ * Library version file
+ *
+ * Author: Paul_Kunz@slac.stanford.edu
+ *
+ */
+
+#include "config.h"
+
+static char const package_string[] = PACKAGE;
+static char const version_string[] = VERSION;
+static char const rcsid[] = "$Id$";
