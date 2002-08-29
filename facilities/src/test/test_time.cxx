6474bd5556a40dbcbf277d5c64b86c793dd2a067
jrb <>
2002-08-29 22:50:30 +0000

    Clean up

--- facilities/src/test/test_time.cxx
+++ facilities/src/test/test_time.cxx
@@ -19,10 +19,7 @@ int main(int argc, char* argv[]) {
     long int       zero = 0;
     long int       negValue = -2000;
     Timestamp unixCreation(zero);
-    //    Timestamp negTimestamp(negValue);
-    //    double    longAgo = 2420000.0;
-    //    Timestamp longAgoTime(longAgo);
-    //    Timestamp veryLongAgo(2200000.0);
+    Timestamp gmttest("1970-01-01 00:00");
     long int        aTime = 4000;
 
     Timestamp aTimestamp(aTime);
@@ -31,14 +28,9 @@ int main(int argc, char* argv[]) {
     std::cout << "unix creation time is " << unixCreation.getString()
               << std::endl;
 
-    //    std::cout << "neg input time is " << negTimestamp.getString()
-    //              << std::endl;
 
     std::cout << "aTimestamp is " << aTimestamp.getString()
               << std::endl;
-
-    //    std::cout << "long ago is " << longAgoTime.getString()
-    //              << std::endl;
   }
   catch (const BadTimeInput e) {
     std::cout << "Exception message:  " << e.complaint << std::endl;
