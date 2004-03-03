e9f08b6ed10fef9cc9204335b9f592a1b2a5cdf5
jchiang <>
2004-03-03 23:21:48 +0000

    move cbuf into outer scope

--- map_tools/src/image/Fits_IO.cxx
+++ map_tools/src/image/Fits_IO.cxx
@@ -388,6 +388,7 @@ void Fits_IO::writeFitsHeader()
         BaseAttr& attr = *(*it);
         void * pval = attr.valuePtr();
         int fitsType=0;
+        char cbuf[120];
 
         if (typeid(attr).name() == doubleAttrClassName)      { fitsType = TDOUBLE; 
         } else if (typeid(attr).name() == floatAttrClassName){ fitsType = TFLOAT; 
@@ -397,7 +398,6 @@ void Fits_IO::writeFitsHeader()
         } else if (typeid(attr).name() == stringAttrClassName){fitsType = TSTRING; 
 
             // special case: need to make a copy of the character string
-            char cbuf[120];
             pval = (void*) cbuf;  // and override the pointer
 
             StringAttr & my_attr = dynamic_cast<StringAttr&>(attr); 
