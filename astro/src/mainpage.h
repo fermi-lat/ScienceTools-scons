f82d76860a6040b6db2764ebc43a415b4baea5bc
burnett <>
2004-06-05 19:31:29 +0000

    clean up a little, adding SkyProj to list

--- astro/src/mainpage.h
+++ astro/src/mainpage.h
@@ -3,10 +3,10 @@
 
 /*! \mainpage package astro
 
-   \authors Gino Tosti, Toby Burnett
+   \authors  Toby Burnett, Sean Robinson, Theodore Hierath, Gino Tosti
 
 
-  This package builds a static library with useful Astronomical utility definitions
+  This package builds a library with useful Astronomical utility definitions
 
   - astro::JulianDate Represent a Julian Date
   - astro::SkyDir Point in the sky
@@ -14,7 +14,9 @@
   - astro::EarthOrbit Dynamics of an Earth orbit
   - astro::SolarSystem Sky positions of Moon and Sun
   - astro::PointingTransform Transformation between GLAST and Celestial coordinate systems
+  - astro::SkyProj Image projections to/from celestial coordinates (wrapper of wcslib)
 
+  
 
     <hr>
   \section notes release notes
