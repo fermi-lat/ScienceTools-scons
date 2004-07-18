f47c8fa59d4fdeee260159e85ad280271a57e273
burnett <>
2004-07-18 22:36:55 +0000

    put in mission start

--- astro/astro/JulianDate.h
+++ astro/astro/JulianDate.h
@@ -1,9 +1,13 @@
+/** @file JulianDate.h
+    @brief definition of JulianDate class
+    
+    $Header$
+
+*/
 
 #ifndef OrbitModel_JulianDate_H
 #define OrbitModel_JulianDate_H
 
-#include <stdio.h>
-#include <math.h>
 #include <string>
 
 namespace astro {
@@ -29,8 +33,8 @@ public:
     * @param utc hours
     */
     JulianDate(int An,int Me,int Gio,double utc);
-    void getGregorianDate(int &An, int &Me, int &Gio, double &utc);
-    std::string getGregorianDate(void);
+    void getGregorianDate(int &An, int &Me, int &Gio, double &utc)const;
+    std::string getGregorianDate(void) const;
 
     //! conversion constructor
     JulianDate(double jd):m_JD(jd){}
@@ -41,6 +45,9 @@ public:
     double seconds()const{ return m_JD* secondsPerDay;}
 
     enum{ secondsPerDay = 60*60*24 };
+
+    /// the GLAST official mission start: 1 Jan 2001 00:00, which should be 2451910.5
+    static JulianDate missionStart(){ return JulianDate(2001,1,1,0); }
     
 private:
     double m_JD;
--- astro/src/test/test.cxx
+++ astro/src/test/test.cxx
@@ -183,10 +183,11 @@ void testJD()
             }
         }
     }
+
     if(passed)
     {
         std::cout << "JD Conversions passed!" << std::endl;
-        std::cout << JD2000.getGregorianDate() << std::endl;
+        std::cout << astro::JulianDate::missionStart().getGregorianDate() << std::endl;
     }
 }
 bool testHTM()
@@ -227,13 +228,13 @@ int main(){
 
         if(! testSkyProj() ) return 1;
 
-        JulianDate JD2000 = JulianDate(2000,1,1,12.); //2451545
+        JulianDate start = JulianDate::missionStart(); 
 
         //   testJD();
 
         double test=0;
 
-        test += fabs(JD2000 -2451545);
+        test += fabs(start -2451910.5);
 
         double ra=30,dec=50;
         SkyDir sd(ra, dec);
@@ -287,7 +288,7 @@ int main(){
             return 0;
         } else {
             cout << "failed a test" << endl;
-            cout << "JD2000" << JD2000 << endl;  
+            cout << "Mission start" << start << endl;  
             cout << "SkyDir("<<ra<<","<<dec<<") " << sd.ra() << ", " << sd.dec()   << endl;
             cout << "SkyDir3("<<l<<","<<b<<") " << sd3.l() << ", " << sd3.b()   << endl;
             cout << "EarthCoordinate("<<lat<<","<<lon<<") " << ec.latitude() << ", " << ec.longitude() << endl;
