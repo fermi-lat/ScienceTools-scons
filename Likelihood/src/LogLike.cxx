ead303bcefb6d3fda91b9305c18d5c5834b7cc34
jchiang <>
2004-08-05 23:06:01 +0000

    try to handle edisp and no edisp cases gracefully

--- Likelihood/src/LogLike.cxx
+++ Likelihood/src/LogLike.cxx
@@ -207,13 +207,32 @@ void LogLike::getEvents(std::string event_file, int) {
          m_events.push_back(thisEvent);
          for (std::vector<std::string>::iterator name = diffuseNames.begin();
               name != diffuseNames.end(); ++name) {
+            std::vector<double> gaussianParams;
             if (ResponseFunctions::useEdisp()) {
-               std::vector<double> gaussianParams;
-               event[*name].get(gaussianParams);
-               m_events.back().setDiffuseResponse(*name, gaussianParams);
+               try {
+                  event[*name].get(gaussianParams);
+                  m_events.back().setDiffuseResponse(*name, gaussianParams);
+               } catch (tip::TipException & eObj) {
+                  std::string message(eObj.what());
+                  if (message.find_first_of("FitsColumn::getVector") ==
+                      std::string::npos) {
+                     throw;
+                  }
+               }
             } else {
-               event[*name].get(respValue);
-               m_events.back().setDiffuseResponse(*name, respValue);
+               try {
+                  event[*name].get(gaussianParams);
+                  m_events.back().setDiffuseResponse(*name, gaussianParams[0]);
+               } catch (tip::TipException &eObj) {
+                  std::string message(eObj.what());
+                  if (message.find_first_of("FitsColumn::getVector") !=
+                      std::string::npos) {
+                     event[*name].get(respValue);
+                     m_events.back().setDiffuseResponse(*name, respValue);
+                  } else {
+                     throw;
+                  }
+               }
             }
          }
       } else {
