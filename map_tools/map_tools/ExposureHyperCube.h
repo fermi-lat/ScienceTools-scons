3dd35632df55958c9f6038db02bb808c54218b19
burnett <>
2005-03-05 15:03:31 +0000

    make sure file gets written.

--- map_tools/map_tools/ExposureHyperCube.h
+++ map_tools/map_tools/ExposureHyperCube.h
@@ -9,7 +9,7 @@
 #ifndef TOOLS_EXPOSUREHYPERCUBE_H
 #define TOOLS_EXPOSUREHYPERCUBE_H
 #include "Exposure.h"
-class BaseImage;
+#include <iostream>
 
 namespace map_tools {
 
@@ -17,23 +17,26 @@ namespace map_tools {
 /** @class ExposureHyperCube 
     @brief Set up an exposure map hypercube
 
-    It is defined as a hypercube in ra, dec, sqrt(1-costheta) bins.
-
     */
 class ExposureHyperCube  {
 public:
     ExposureHyperCube( const Exposure& exp, std::string outfile)
-        :m_exposure(exp),m_outfile(outfile)
+        :m_exposure(exp),m_outfile(outfile), m_written(false)
     {
     }
     void save()
     {
-        m_exposure.write(m_outfile);
+        if(!m_written) m_exposure.write(m_outfile);
+        m_written=true;
     }
 
-    ~ExposureHyperCube(){}
+    ~ExposureHyperCube()
+    {
+        save();
+    }
     const Exposure& m_exposure;
     std::string m_outfile;
+    bool m_written;
 };
 }// namespace map_tools
 #endif //TOOLS_EXPOSUREHYPERCUBE_H
