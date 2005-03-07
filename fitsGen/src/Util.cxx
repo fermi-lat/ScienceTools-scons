04320c80566b924e956df6e817b0e146a15eb0b1
jchiang <>
2005-03-07 19:06:06 +0000

    set mission start = mjdref; tagging v1r1p3

--- fitsGen/src/Util.cxx
+++ fitsGen/src/Util.cxx
@@ -53,7 +53,7 @@ void Util::writeDateKeywords(tip::Table * table, double start_time,
       header["DATE"].set(current_time.getGregorianDate());
    } catch (...) {
    }
-   astro::JulianDate mission_start(2005, 7, 18, 0);
+   astro::JulianDate mission_start(2006, 12, 31, 23.99888);
    astro::JulianDate date_start(mission_start + start_time/secsPerDay);
    astro::JulianDate date_stop(mission_start + stop_time/secsPerDay);
    try {
