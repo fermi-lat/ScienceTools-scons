3172041304d73bca73724b6c50d6e54cb65c28e3
burnett <>
2005-06-16 13:17:39 +0000

    fix test program

--- flux/src/test/testMgr.cxx
+++ flux/src/test/testMgr.cxx
@@ -75,7 +75,7 @@ void galacticTest(FluxMgr* fm, std::string sourceName,double count){
         //of fluxsource, and is expected.
         time+=interval;
         fm->pass(interval);
-        Hep3Vector abc(fm->transformToGlast(time,GPS::CELESTIAL).inverse()*(-(f->launchDir())));
+        Hep3Vector abc(fm->transformToGlast(time,astro::GPS::CELESTIAL).inverse()*(-(f->launchDir())));
         astro::SkyDir dir(abc,astro::SkyDir::EQUATORIAL);
         std::cout << "particle "<< i << " located at (l,b) = " << dir.l() << "," << dir.b() << std::endl;
         lavg +=dir.l();
