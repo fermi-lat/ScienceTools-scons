636f158283b239ccdb5d49644be435d3e484b5bd
jrb <>
2005-10-24 22:24:26 +0000

    Add new utility Util::utoa, provides string representation for unsigned int Also get rid of no-longer-necessary references to DEFECT_NO_STRINGSTREAM

--- facilities/src/Util.cxx
+++ facilities/src/Util.cxx
@@ -2,11 +2,11 @@
 
 #include "facilities/Util.h"
 
-#ifdef DEFECT_NO_STRINGSTREAM
-#include <strstream>
-#else
+//#ifdef DEFECT_NO_STRINGSTREAM
+//#include <strstream>
+//#else
 #include <sstream>
-#endif
+//#endif
 
 #include <iostream>
 #include <cstdio>
@@ -63,24 +63,28 @@ namespace facilities {
     // Purpose and Method:  Provide a standard routine to convert integers
     //    into std::string.  The method used depends upon the availability of
     //    the stringstream classes.  The stringstream classes are the
-    //    standard, but some compilers do yet support them.
-    //    The method used is determined by the DEFECT_NO_STRINGSTREAM
-    //    macro, defined in the facilities requirements file.
+    //    standard, but some compilers do yet support them. 
+    //    [Oct. 24, 2005. Compiler upgrades some time ago eliminated this
+    //    problem, so no longer need the #ifdef
 
     static char outCharPtr[20];
 
-#ifdef DEFECT_NO_STRINGSTREAM
-    // Using static buffer to avoid problems with memory allocation
-    char a[100]=""; 
-    std::ostrstream locStream(a,100);
-#else
     std::ostringstream locStream;
     locStream.str("");
-#endif
+
     locStream << val;
-#ifdef DEFECT_NO_STRINGSTREAM
-    locStream << std::ends;
-#endif
+
+    outStr = locStream.str();
+    strcpy(outCharPtr, outStr.c_str());
+    return outCharPtr;
+  }
+
+  const char* Util::utoa(unsigned int val, std::string &outStr) {
+    static char outCharPtr[20];
+
+    std::ostringstream locStream;
+    locStream.str("");
+    locStream << val;
     outStr = locStream.str();
     strcpy(outCharPtr, outStr.c_str());
     return outCharPtr;
@@ -98,11 +102,8 @@ namespace facilities {
 
     int val;
 
-#ifdef DEFECT_NO_STRINGSTREAM
-    std::istrstream locStream(inStr.c_str());
-#else
     std::istringstream locStream(inStr);
-#endif
+
     locStream >> val;
     if (!locStream) {return 0;}
     return val;
