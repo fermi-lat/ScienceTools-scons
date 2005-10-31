6c94f64badeb918be3e9e449818b74ffa6c5b638
jchiang <>
2005-10-31 01:32:24 +0000

    refactor to be consistent with SourcePopulation input

--- likeGui/python/ObsSim/create_library.py
+++ likeGui/python/ObsSim/create_library.py
@@ -19,8 +19,8 @@ _ptsrc_template = """<source name="%s" flux="%.3e">
 </source>
 """
 
-def ptsrc(name, flux, ra, dec, gamma, emin=20, emax=1e6,
-          gamma2=None, ebreak=2e3):
+def ptsrc(name, ra, dec, flux, gamma, gamma2=None, ebreak=2e3,
+          emin=20, emax=1e6):
     if gamma2 is not None:
         bpl = 'gamma2="%.2f" ebreak="%.1f" ' % (float(gamma2), float(ebreak))
     else:
@@ -52,8 +52,17 @@ def makeSrcLib(inputfile):
     return outlines
 
 if __name__ == '__main__':
-    infile = "srclib.txt"
+    import sys
+    if sys.argv[1:2]:
+        infile = sys.argv[1]
+    else:
+        infile = "srclib.txt"
     items = makeSrcLib(infile)
-    for item in items:
-        print item
-        
+    if sys.argv[2:3]:
+        outfile = open(sys.argv[2], 'w')
+        for item in items:
+            outfile.write(item + '\n')
+        outfile.close()
+    else:
+        for item in items:
+            print item
