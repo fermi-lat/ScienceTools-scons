a6fbe378e9fc34ce1d437dc492de8685defb5360
jchiang <>
2006-01-06 22:46:32 +0000

    fix offset problem for DATE keywords in FT1

--- observationSim/src/EventContainer.cxx
+++ observationSim/src/EventContainer.cxx
@@ -236,12 +236,13 @@ void EventContainer::writeEvents(double obsStopTime) {
 
 // Set stop time to be arrival time of last event if obsStopTime is
 // negative (i.e., not set);
-   double stop_time(m_events.back().time());
+   double stop_time(m_events.back().time() - Spectrum::startTime());
    if (obsStopTime > 0) {
       stop_time = obsStopTime;
    }
-   ft1.setObsTimes(m_startTime, stop_time);
-
+   ft1.setObsTimes(m_startTime + Spectrum::startTime(),
+                   stop_time + Spectrum::startTime());
+   
    dataSubselector::Cuts * cuts;
    if (m_cuts) {
       cuts = new dataSubselector::Cuts(*m_cuts);
@@ -269,7 +270,7 @@ void EventContainer::writeEvents(double obsStopTime) {
    m_fileNum++;
 
 // Set the start time for next output file to be current stop time.
-   m_startTime = stop_time + Spectrum::startTime();
+   m_startTime = stop_time;
 }
 
 } // namespace observationSim
