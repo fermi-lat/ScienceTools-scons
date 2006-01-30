d092296ac1624556971791ea36a6f297bb54ba6b
peachey <>
2006-01-30 14:46:54 +0000

    Add summary for doxygen.

---
+++ gtgraph/src/mainpage.h
@@ -0,0 +1,8 @@
+/** \file mainpage.h
+    \brief Summary of gtgraph package.
+    \author James Peachey, GSSC/HEASARC/GSFC/NASA
+
+Gtgraph is a rudimentary package for plotting arbitrary combinations
+of fits columns in 2 and 3 dimensional plots. This was originally
+a test application for st_graph. It is not under active development.
+*/
