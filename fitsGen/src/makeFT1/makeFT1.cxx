4e5d82ba17ff4b3acf35582e11fffd808143d881
jchiang <>
2006-02-02 17:49:14 +0000

    slight makeFT1 update; tagging v2r2p2

--- fitsGen/src/makeFT1/makeFT1.cxx
+++ fitsGen/src/makeFT1/makeFT1.cxx
@@ -28,14 +28,14 @@
 using namespace fitsGen;
 
 int main(int iargc, char * argv[]) {
-   std::string rootFile("merit.root");
-   std::string fitsFile("myLatData.fits");
-   
-   Util::getFileNames(iargc, argv, rootFile, fitsFile);
-   if (iargc == 1) {
-      std::cout << "input merit file: " << rootFile << "\n"
-                << "output FT1 file: " << fitsFile << std::endl;
+   if (iargc < 3) {
+      std::cout << "usage: <merit file> <FT1 file> [<TCut>]"
+                << std::endl;
+      std::exit(1);
    }
+
+   std::string rootFile(argv[1]);
+   std::string fitsFile(argv[2]);
 
    std::ostringstream filter;
    if (iargc == 4) {
@@ -66,8 +66,6 @@ int main(int iargc, char * argv[]) {
          double dec = merit["FT1Dec"];
          ft1["ra"].set(ra);
          ft1["dec"].set(dec);
-//          ft1["l"].set(merit["FT1L"]);
-//          ft1["b"].set(merit["FT1B"]);
          astro::SkyDir dir(ra, dec);
          ft1["l"].set(dir.l());
          ft1["b"].set(dir.b());
@@ -84,7 +82,7 @@ int main(int iargc, char * argv[]) {
             ft1["event_class"].set(1);
             ft1["conversion_type"].set(1);
          }
-//         ft1["livetime"].set(merit["FT1Livetime"]);
+         ft1["livetime"].set(merit["FT1Livetime"]);
          ncount++;
       }
       std::cout << "number of rows processed: " << ncount << std::endl;
