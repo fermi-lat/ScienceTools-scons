386140821ebfd5cb22d3d33640d9f133362a89d2
jrb <>
2006-04-14 17:49:14 +0000

    Add optional arg. to Util::utoa to support hex output

--- facilities/src/Util.cxx
+++ facilities/src/Util.cxx
@@ -79,12 +79,19 @@ namespace facilities {
     return outCharPtr;
   }
 
-  const char* Util::utoa(unsigned int val, std::string &outStr) {
+  const char* Util::utoa(unsigned int val, std::string &outStr, int base) {
     static char outCharPtr[20];
 
     std::ostringstream locStream;
     locStream.str("");
+    if (base == 16) {
+      locStream.fill('0');
+      locStream.width(8);
+      locStream.setf(std::ios::hex);
+      locStream.unsetf(std::ios::dec);
+    }
     locStream << val;
+
     outStr = locStream.str();
     strcpy(outCharPtr, outStr.c_str());
     return outCharPtr;
