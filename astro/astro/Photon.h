547f1df1104c069bdeb1e561abb59f04b172e839
burnett <>
2006-06-28 20:25:40 +0000

    add source id

--- astro/astro/Photon.h
+++ astro/astro/Photon.h
@@ -19,20 +19,23 @@ namespace astro {
     */
     class Photon : public astro::SkyDir {
     public:
-        Photon(const astro::SkyDir& dir, double energy, double time, int event_class=-1)
+        Photon(const astro::SkyDir& dir, double energy, double time, int event_class=-1, int source=-1)
             : astro::SkyDir(dir)
             , m_energy(energy)
             , m_time(time)
             , m_event_class(event_class)
+            , m_source(source)
         {}
         double energy()const{return m_energy;}
         int eventClass()const{return m_event_class;}
         double time()const{return m_time;}
+        int source()const{return m_source;}
 
     private:
         double m_energy;
         double m_time;
         int m_event_class;
+        int m_source;
 
     };
 
