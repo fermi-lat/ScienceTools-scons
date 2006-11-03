ba3d8cfb77015c171cce19b077ed23e924c1e546
jchiang <>
2006-11-03 21:18:44 +0000

    workaround for time query beyond end of ft2 file problem.

--- observationSim/src/Simulator.cxx
+++ observationSim/src/Simulator.cxx
@@ -194,7 +194,7 @@ void Simulator::makeEvents(EventContainer &events,
 
 // Enclose loop in outer try block, catching a GPS-thrown exception when
 // time exceeds pointing history database.
-   try {
+  try {
 // Loop over event generation steps until done.
       while (!done()) {
 
@@ -242,13 +242,17 @@ void Simulator::makeEvents(EventContainer &events,
          }
       } // while (!done())
    } catch (std::exception & eObj) {
-      if ( (!st_facilities::Util::expectedException(eObj,"Time out of Range!")
-            && !st_facilities::Util::expectedException(eObj,
-               "time is beyond end of history file"))
-// This latter check is an ugly kluge necessary because one does not (yet)
-// have access to the start and stop times of the pointing history via
-// astro's GPS:
-          || (m_useSimTime && m_elapsedTime < 0.9*m_simTime)) {
+//       if ( (!st_facilities::Util::expectedException(eObj,"Time out of Range!")
+//             && !st_facilities::Util::expectedException(eObj,
+//                "time is beyond end of history file"))
+// // This latter check is an ugly kluge necessary because one does not (yet)
+// // have access to the start and stop times of the pointing history via
+// // astro's GPS:
+//           || (m_useSimTime && m_elapsedTime < 0.9*m_simTime)) {
+//          throw;
+//       }
+      if (!st_facilities::Util::
+          expectedException(eObj, "time is beyond end of history file")) {
          throw;
       }
    }
