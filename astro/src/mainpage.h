02da5caa59bcf029ac9f148e55d6c8eae306366e
burnett <>
2006-11-05 20:06:27 +0000

    massive refactoring of the GPS class. Remove lots of obsolete, or not maintained features, split history management to new class PointingHistory, and represention of position/orientaion to PointingInfo.

--- astro/src/mainpage.h
+++ astro/src/mainpage.h
@@ -16,9 +16,8 @@
   - astro::PointingTransform Transformation between GLAST and Celestial coordinate systems
   - astro::SkyProj Image projections to/from celestial coordinates (wrapper of wcslib)
   - astro::Healpix manage transformation to/from Healpix pixels, as an STL container
-  - astro::HTM   same for HTM pixels
-
-  
+  - astro::HealPixel Special class to support multiple pixel sizes 
+  - astro::Quaternion Quaternion objects, used to represent rotations
 
     <hr>
   \section notes release notes
