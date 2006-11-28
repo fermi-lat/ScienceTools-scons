4e301249a1c88c04904a141d8c426715fd049b83
burnett <>
2006-11-28 03:03:12 +0000

    initial import

---
+++ embed_python/src/mainpage.h
@@ -0,0 +1,19 @@
+// $Header$
+// Mainpage for doxygen
+
+/*! \mainpage package embed_python
+
+   \authors  Toby Burnett
+
+
+  This package builds a library to facilitate embedding python.
+
+
+    <hr>
+  \section notes release notes
+  release.notes
+  \section requirements requirements
+  \include requirements
+
+*/
+
