6fd0ed9c57a8f7f169c6c40742c21cf173b2b3db
burnett <>
2007-06-17 03:54:26 +0000

    new ctor with argv use.

--- embed_python/embed_python/Module.h
+++ embed_python/embed_python/Module.h
@@ -21,10 +21,23 @@ namespace embed_python {
 
     @author T.Burnett
 
+    Acknowledgement: valuable contributions from J. Chiang.
+
 */
 class Module {
 public:
 
+           
+    /// @param python_dir Directory to be prepended to PYTHONPATH
+    /// @param module name of module to load, without ".py". 
+    /// @param argc,argv command-line args: will set sys.argv, which will then be 
+    ///        available in the setup module
+    Module(const std::string & python_dir,
+        const std::string& module, 
+        int argc, char** argv);
+
+    /// @brief This constructor is deprecated! Please don't use. It is here
+    //         to avoid critical notes about breaking interfaces
     /// @param path path to module. If empty, assume cwd
     /// @param module name of module to load, without ".py". 
     /// @param python_dir Directory to be prepended to PYTHONPATH
@@ -32,7 +45,7 @@ public:
     Module(const std::string& path, const std::string& module, 
            const std::string & python_dir="",
            bool verbose=false);
-           
+
     ~Module();
 
     /// return a numeric type
