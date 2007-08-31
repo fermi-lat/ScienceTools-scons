a771dd1867de7ba0df93342c5b6eb3c1ae13bd16
burnett <>
2007-08-31 04:24:41 +0000

    tagging after fixes for pointfit default

--- pointlike/python/pointfind_setup.py
+++ pointlike/python/pointfind_setup.py
@@ -1,23 +1,29 @@
 #  setup for pointlike source finder
 # $Header$
-print 'setup for pointfind'
+print 'runing setup for pointfind'
 
 # data source 
 
-pixelfile = "F:/glast/data/SC2/obssim/sc2_obssim_map.fits"
+def test():
+  " define the pixelfile for a quick test, running the pixel file created by the test program"
+  import os
+  path = os.environ['POINTLIKEROOT']
+  return os.path.join(path, 'src', 'test', 'pointlike_test.fits')
+
+pixelfile = test()
 
 # direction and cone or radius about it to examine
-l,b = 0, -90
+l,b = 0, 0
 
-radius   = 60 # 180 for all sky
+radius   = 180 # 180 for all sky
 
-count_threshold=346  
+count_threshold=346  # this number is a bit arbitray
 
 TSmin   = 10   # minimum TS for candidates
 
-# parameter for pruning: radius in degrees
+# parameter for pruning: radius in degrees (also needs to be tuned)
 prune_radius = 0.25
 
 # file to write a table of results to
-outfile='south_sources.txt'
+outfile='pointfind_test.txt'
         
