aad3e51bcec217892eec7a9972fb3484a61a9954
jchiang <>
2007-10-20 15:55:24 +0000

    use st_facilities::FitsUtil::fcopy and angsep(...); tagging v5r6p2

--- dataSubselector/src/SkyConeCut.cxx
+++ dataSubselector/src/SkyConeCut.cxx
@@ -115,18 +115,11 @@ bool SkyConeCut::accept(double ra, double dec) const {
 }
 
 std::string SkyConeCut::filterString() const {
-   static double DEG_TO_RAD(M_PI/180.);
    double ra(m_coneCenter.ra());
    double dec(m_coneCenter.dec());
    std::ostringstream q;
    q << std::setprecision(10);
-   q << "((2*asin(min(1,sqrt(max(0,(sin((DEC-" << dec << ")*" 
-     << DEG_TO_RAD/2 << ")*sin((DEC-" << dec << ")*" 
-     << DEG_TO_RAD/2 << "))+(cos(DEC*" << DEG_TO_RAD
-     << ")*" << std::cos(dec*DEG_TO_RAD) << "*sin((RA-" 
-     << ra << ")*" << DEG_TO_RAD/2 << ")*sin((RA-" 
-     << ra << ")*" << DEG_TO_RAD/2 << ")))))))<" 
-     << m_radius*DEG_TO_RAD << ")";
+   q << "angsep(RA,DEC," << ra << "," << dec << ") < " << m_radius;
    return q.str();
 }
 
