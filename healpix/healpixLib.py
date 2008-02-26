d20f60d1cf3f329a52ae93fb73ee9b3c6c173028
glastrm <>
2008-02-26 03:31:51 +0000

    Set lib dependencies up for when building libraries

--- healpix/healpixLib.py
+++ healpix/healpixLib.py
@@ -1,6 +1,7 @@
 #$Id$
 def generate(env, **kw):
-    env.Tool('addLibrary', library = ['healpix'])
+    if not kw.get('depsOnly',0):
+        env.Tool('addLibrary', library = ['healpix'])
     env.Tool('astroLib')
     
 def exists(env):
