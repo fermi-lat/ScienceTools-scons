f15216941b1944a6c38f8e6f240b661db44d812f
jurgen <>
2008-05-06 16:00:08 +0000

    Import srcid.py script and classes definition into CVS

---
+++ sourceIdentify/python/classes/blazar.py
@@ -0,0 +1,34 @@
+#--------------------------------------------------------------------------------------------
+# Source class: Blazars
+#--------------------------------------------------------------------------------------------
+#
+# $Author$
+# $Revision$
+# $Date$
+#--------------------------------------------------------------------------------------------
+# catid:            Catalogue Identifier (prefixes results and determines result name)
+# catname:          Catalogue FITS name
+# new_quantity:     List of new quantities
+# selection:        List of selection criteria
+# prob_method:      Probability method
+# prob_prior:       Prior probability
+# prob_thres:       Probability threshold
+# max_counterparts: Maximum number of counterparts
+#--------------------------------------------------------------------------------------------
+"""
+Blazar Source Class (CGRaBS).
+"""
+
+# Catalogue identifier
+catid = "BLZ"
+
+# Catalogue FITS filename
+catname = "obj-blazar.fits"
+
+# Probability method and threshold
+prob_method = "PROB_POST"
+prob_prior  = 0.01
+prob_thres  = 0.10
+
+# Maximum number of counterparts
+max_counterparts = 10
