164b523a17bf82963d377bc9cb0c99d149cda0e1
burnett <>
2008-05-28 21:41:53 +0000

    current setup for testing

--- pointlike/python/pointlike_defaults.py
+++ pointlike/python/pointlike_defaults.py
@@ -30,8 +30,7 @@ class Diffuse:
 class PointSourceLikelihood: #parameters for the likelihood calculation
     # HEALpix level range for energy band  fits
 
-    minlevel=8   # minimum level to use for fits (>-6)  
-    maxlevel=13  # maximum level for fits  (<=13)
+    emin = 700;   # only select bands including or above this energy
     minalpha=0.15# minimum value for signal fraction to use in TS total
 
     # parameters governing iteration cycle of bands/position
@@ -53,10 +52,10 @@ class PointSourceLikelihood: #parameters for the likelihood calculation
 class SourceFinder:  # parameters for the SourceFinder.
 
     TSmin = 8.0          # overall minimum TS
-    pixLevel = 8         # pixelization level for first pass.  Higher number -> more points tested for possible sources
-    pixel_fraction = 0.2 # fraction of pixels to sample, sorted according to weighted photon count
-    group_radius = 1.0   # radius for making groups
+    pixel_fraction = 1.0 # fraction of pixels to sample, sorted according to weighted photon count
     prune_radius =0.25   # pruning radius in degrees (also needs to be tuned)
+    group_radius = 4.0   # maximum radius for nearby sources.
     examine_radius=180   # default is to examine full sky
+    pass1_nside=256      #
             
 
