1b94821f67b548a9ce5b10e200cf0f48689f7893
burnett <>
2008-12-30 17:47:23 +0000

    change a critical int to long. (??) and tag

--- embed_python/src/Module.cxx
+++ embed_python/src/Module.cxx
@@ -152,7 +152,7 @@ Module::~Module()
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 PyObject* Module::attribute(const std::string& name, bool check)const
 {
-    unsigned int c (name.find_first_of("."));
+    unsigned long c (name.find_first_of("."));
     if( c != std::string::npos) {
         PyObject * t = attribute(name.substr(0,c));
         PyObject * ret = PyObject_GetAttrString(t, const_cast<char*>(name.substr(c+1).c_str()));
