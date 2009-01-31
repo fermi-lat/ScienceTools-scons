f402b34632ab430369a3811081a3f35c048840e8
markusa <>
2009-01-31 00:32:43 +0000

    fixes for spectral fitting

--- pointlike/pointlike/ExtendedLikelihood.h
+++ pointlike/pointlike/ExtendedLikelihood.h
@@ -96,7 +96,6 @@ namespace pointlike {
 
     std::pair<double,double> flux() const ;
     double exposure(double E = -1) const ;
-    double full_exposure(double E,int combined) const ;
     void setFlux(double flux);
     
     double sigma_alpha() const { return m_sigma_alpha;}
