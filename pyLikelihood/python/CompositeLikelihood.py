adb8f84e313c84ebc27c691f140a03d1a339f9af
jchiang <>
2009-03-31 18:20:54 +0000

    set tolType in each call to Optimizer::find_min

--- pyLikelihood/python/CompositeLikelihood.py
+++ pyLikelihood/python/CompositeLikelihood.py
@@ -15,14 +15,18 @@ class CompositeLikelihood(object):
         self.composite = pyLike.CompositeLikelihood()
         self.srcNames = []
         self.components = []
+        self.tolType = pyLike.ABSOLUTE
+        self.tol = 1e-2
     def addComponent(self, srcName, like):
         self.composite.addComponent(srcName, like.logLike)
         self.srcNames.append(srcName)
         self.components.append(like)
-    def fit(self, verbosity=2, tol=1e-5, optimizer='Minuit'):
+    def fit(self, verbosity=2, tol=None, optimizer='Minuit'):
         myOpt = pyLike.OptimizerFactory_instance().create(optimizer,
                                                           self.composite)
-        myOpt.find_min(verbosity, tol)
+        if tol is None:
+            tol = self.tol
+        myOpt.find_min(verbosity, tol, self.tolType)
     def __getattr__(self, attrname):
         return getattr(self.composite, attrname)
     def __repr__(self):
