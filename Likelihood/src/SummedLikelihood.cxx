d3f0acedadd1a16434c22979c1bf534c48ee1050
jchiang <>
2009-06-12 20:04:13 +0000

    const correctness fix

--- Likelihood/src/SummedLikelihood.cxx
+++ Likelihood/src/SummedLikelihood.cxx
@@ -67,7 +67,7 @@ void SummedLikelihood::getFreeDerivs(std::vector<double> & derivs) const {
 // Build vector of component parameter names.  This list should have
 // the same ordering as the derivatives wrt the free parameters.
    m_components.front()->getFreeDerivs(derivs);
-   for (ComponentIterator_t it(m_components.begin() + 1);
+   for (ComponentConstIterator_t it(m_components.begin() + 1);
         it != m_components.end(); ++it) {
       std::vector<double> freeDerivs;
       (*it)->getFreeDerivs(freeDerivs);
