8f9586a0ed6a59ff7d52e40b7be1a9545ecb2a3f
jchiang <>
2009-10-23 20:44:23 +0000

    trap errors from reading xml model file and print more user-friendly error message

--- Likelihood/src/SourceModel.cxx
+++ Likelihood/src/SourceModel.cxx
@@ -318,12 +318,22 @@ void SourceModel::readXml(std::string xmlFile,
 
 // Create a SourceFactory to read in the xml file.
    SourceFactory srcFactory(m_observation);
+//    try {
+//       srcFactory.readXml(xmlFile, funcFactory, requireExposure,
+//                          addPointSources);
+//    } catch (xmlBase::DomException & eObj) {
+//       m_formatter->err() << "SourceModel::readXml:\n DomException: " 
+//                          << eObj.what() << std::endl;
+//    }
    try {
       srcFactory.readXml(xmlFile, funcFactory, requireExposure,
                          addPointSources);
-   } catch (xmlBase::DomException & eObj) {
-      m_formatter->err() << "SourceModel::readXml:\n DomException: " 
-                         << eObj.what() << std::endl;
+   } catch (...) {
+      std::ostringstream message;
+      message << "\nError reading in the xml model file.\n"
+              << "Please check that you are using the correct xml "
+              << "format for this tool." << std::endl;
+      throw Exception(message.str());
    }
 
 // Loop over the sources that are now contained in srcFactory and add
