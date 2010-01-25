8865150a1937837bb2e2d3af3b4f1b134e8da79f
jchiang <>
2010-01-25 22:20:25 +0000

    use stringent emin, emax limits in Source::Npred(double, double).  loose limits cause problems for plotting Npred in narrow ROI energy bands

--- Likelihood/src/Source.cxx
+++ Likelihood/src/Source.cxx
@@ -50,12 +50,12 @@ double Source::Npred() {
 
 double Source::Npred(double emin, double emax) const {
    const std::vector<double> & energies = m_observation->roiCuts().energies();
-   if (fabs((emin - energies.front())/emin) < 1e-2) {
-      emin = energies.front();
-   }
-   if (fabs((emax - energies.back())/emax) < 1e-2) {
-      emax = energies.back();
-   }
+//    if (fabs((emin - energies.front())/emin) < 1e-2) {
+//       emin = energies.front();
+//    }
+//    if (fabs((emax - energies.back())/emax) < 1e-2) {
+//       emax = energies.back();
+//    }
    if (emin < energies.front() || emax > energies.back()) {
       throw std::out_of_range("Source::Npred(emin, emax)");
    }
