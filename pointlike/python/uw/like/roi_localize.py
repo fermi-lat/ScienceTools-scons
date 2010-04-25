102032f13d0ac422a61f4d1015310677bdf7ffc9
burnett <>
2010-04-25 01:50:25 +0000

    have the localize function return more useful details about the fit

--- pointlike/python/uw/like/roi_localize.py
+++ pointlike/python/uw/like/roi_localize.py
@@ -60,7 +60,7 @@ class ROILocalizer(object):
    def localize(self):
       """Localize a source using an elliptic approximation to the likelihood surface.
 
-         return fit position
+         return fit position, number of iterations, distance moved, delta TS
       """
       roi   = self.roi
       which = self.which
@@ -115,7 +115,7 @@ class ROILocalizer(object):
       roi.ldir    = l.dir
       roi.lsigma  = l.sigma
       roi.delta_loc_logl = (ll0 - ll1)
-      return l.dir
+      return l.dir, i, delt, 2*(ll0-ll1)
 
    def spatialLikelihood(self,skydir,update=False,which=0,bandfits=False):
       """Calculate log likelihood as a function of position a point source.
