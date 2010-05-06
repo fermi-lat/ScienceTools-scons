8544e63f09d21a6dd7ad0617386a10bb78b98d36
jchiang <>
2010-05-06 17:58:23 +0000

    remove zmax!=180 warning and let users live with the consequences

--- Likelihood/src/makeExposureCube/makeExposureCube.cxx
+++ Likelihood/src/makeExposureCube/makeExposureCube.cxx
@@ -211,15 +211,6 @@ void ExposureCube::createDataCube() {
    formatter.info(4) << "applying filter: " << filter.str() << std::endl;
 
    double zmax = m_pars["zmax"];
-   if (zmax < 180.) {
-      formatter.info(2) << "WARNING: You have chosen to apply a zenith angle cut of "
-                        << zmax << " degrees." << std::endl
-                        << "Applying such a cut for this tool is not equivalent to \n"
-                        << "applying a zenith angle cut in gtselect." << std::endl
-                        << "If you don't understand this comment, " << std::endl
-                        << "then you probably shouldn't be applying this cut." 
-                        << std::endl;
-   }
 
    // Set the number of phibins using the static function interface
    // from healpix::CosineBinner (this is how
