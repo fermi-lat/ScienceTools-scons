a570b0df6df5d30868915bd2ea539a28b66ed1da
burnett <>
2010-06-10 21:37:19 +0000

    implement pivot energy calculation for PowerLaw; also full_name() function to include e0 information

--- pointlike/python/uw/like/Models.py
+++ pointlike/python/uw/like/Models.py
@@ -296,6 +296,9 @@ Optional keyword arguments:
          lo.p[i] += errs[i]
 
       return N.asarray(derivs)
+      
+   def full_name(self):
+      return self.pretty_name if self.pretty_name != 'PowerLaw' else 'PowerLaw, e0=%.0f'% self.e0
 
 #===============================================================================================#
 
@@ -320,6 +323,16 @@ Spectral parameters:
       n0,gamma = 10**self.p
       f = (n0/self.flux_scale)*(self.e0/e)**(gamma-self.index_offset)
       return N.asarray([f/n0,f*N.log(self.e0/e)])
+
+   def pivot_energy(self):
+      """ assuming a fit was done, estimate the pivot energy 
+           (This only implemented for this model)
+      """
+      A  = 10**self.p[0]
+      C = self.get_cov_matrix()
+      if C[1,1]==0:
+         raise Exception('PowerLaw fit required before calculating pivot energy')
+      return self.e0*N.exp( C[0,1]/(A*C[1,1]) )
 
 #===============================================================================================#
 
