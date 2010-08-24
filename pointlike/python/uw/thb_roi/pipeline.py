136e4648c4da9f2879edd088550f23d5d8ad657d
burnett <>
2010-08-24 22:02:20 +0000

    some protection

--- pointlike/python/uw/thb_roi/pipeline.py
+++ pointlike/python/uw/thb_roi/pipeline.py
@@ -36,7 +36,7 @@ class Pipeline(object):
                 roifig    = False,
                 sedfig    = True,
                 bgfree    =  [True,False,True],
-                fit_method= 'minuit',
+                fit_method= 'simplex',
                 seeds     = None,
                 seeds_only= False,
                 sed_limits= None, 
@@ -101,7 +101,7 @@ class Pipeline(object):
         r = self.factory([(name, sdir)], bgfree=self.bgfree) #use default model parameters
 
         # if a different direction, we need to disable the original, most likely the nearest
-        if r.psm.point_sources[1].name.strip() == '%s' % r.name.strip():
+        if len(r.psm.point_sources)>1 and r.psm.point_sources[1].name.strip() == '%s' % r.name.strip():
             r.psm.models[1].p[0]=-20
             print '-----> disabled catalog source with same name found in background <-------'
         r.dump(maxdist=2)
@@ -134,6 +134,11 @@ class Pipeline(object):
                 print 'failed'
                 psig = 0.5; ellipse = (psig,psig,0) #default 
                 tsmaxpos = r.center # just for deltata below
+        if psig>0.5 or not localized: #protection
+            oldsig = psig
+            psig = 0.5; ellipse = (psig,psig,0)
+            print 'warning: too large error ellipse, %.1f deg, cut to %.1f' % (oldsig, psig)
+            
 
         print '\n========   ASSOCIATION  =============='
         adict = self.associate(name, sdir, ellipse) if self.associate else None
