e6a9d967cb477174e4112d1cacb864a006f15377
jrb <>
2010-10-05 19:24:19 +0000

    Add -rdynamic link option to environment when linking against this library on Linux

--- embed_python/embed_pythonLib.py
+++ embed_python/embed_pythonLib.py
@@ -2,6 +2,8 @@
 def generate(env, **kw):
     if not kw.get('depsOnly',0):
         env.Tool('addLibrary', library = ['embed_python'])
+	if env['PLATFORM'] == 'posix':
+            env.AppendUnique(LINKFLAGS	= ['-rdynamic'])	
     env.Tool('addLibrary', library = env['pythonLibs'])
 
 def exists(env):
