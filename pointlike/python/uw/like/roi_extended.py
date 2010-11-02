c080c915e72001639e6d5058cf600e4d9a7b7674
lande <>
2010-11-02 06:38:10 +0000

    poor variable name fixed.

--- pointlike/python/uw/like/roi_extended.py
+++ pointlike/python/uw/like/roi_extended.py
@@ -151,8 +151,8 @@ class ROIExtendedModel(ROIDiffuseModel):
 
             self.set_state(en,band.ct,band)
 
-            exp=band.exp.value
-            myband.er = exp(es.spatial_model.center,en)/exp(rd,en)
+            exposure=band.exp.value
+            myband.er = exposure(es.spatial_model.center,en)/exposure(rd,en)
 
             myband.pix_counts = self._pix_value(band.wsdl)
             myband.pix_counts *= band.b.pixelArea()
