0eda4fe4c401f8b50c8469cb21f640489b011230
kerrm <>
2010-11-26 18:11:57 +0000

    Changed default SkyDir in print_summary to be ROI center explicitly.

--- pointlike/python/uw/like/roi_analysis.py
+++ pointlike/python/uw/like/roi_analysis.py
@@ -738,14 +738,13 @@ class ROIAnalysis(object):
         Parameters
         ----------
              sdir : SkyDir, optional, default None for center
-                   for center: default will be the first source
              galactic : bool, optional, default False
                  set true for l,b
              maxdist : float, optional, default 5
                  radius in degrees
 
         """
-        if sdir is None: sdir = self.psm.point_sources[0].skydir
+        if sdir is None: sdir = self.roi_dir
         print '\n\t Nearby sources within %.1f degrees %s' % (maxdist,title)
         colstring = 'name dist ra dec flux8 index cutoff'
         if galactic: colstring =colstring.replace('ra dec', 'l b')
