6afa4e6fa9c0d87afc63bac09a9ec9586fcfed6e
cohen <>
2011-01-28 09:15:00 +0000

    finish off Josh fix to pycaldb

--- pointlike/python/uw/like/pycaldb.py
+++ pointlike/python/uw/like/pycaldb.py
@@ -119,8 +119,8 @@ class CALDBManager(object):
             return
 
         # try to read form caldb index
-        front=N.where((N.char.find(self.irf_names,irf)!=-1)&(self.irf_types=='EFF_AREA')&(self.conv_types=='FRONT'))[0]
-        back=N.where((N.char.find(self.irf_names,irf)!=-1)&(self.irf_types=='EFF_AREA')&(self.conv_types=='BACK'))[0]
+        front=N.where((N.chararray.find(self.irf_names,irf)!=-1)&(self.irf_types=='EFF_AREA')&(self.conv_types=='FRONT'))[0]
+        back=N.where((N.chararray.find(self.irf_names,irf)!=-1)&(self.irf_types=='EFF_AREA')&(self.conv_types=='BACK'))[0]
 
         # if front & back exist in the caldb.indx
         if len(front)==1 and len(back)==1:
