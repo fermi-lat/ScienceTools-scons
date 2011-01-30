ce6668106e4a5463b83eafec08376541b07639fb
burnett <>
2011-01-30 18:40:51 +0000

    move printout  of count setup to loop doing it to guage timimg.

--- pointlike/python/uw/like/roi_managers.py
+++ pointlike/python/uw/like/roi_managers.py
@@ -284,11 +284,6 @@ class ROIDiffuseManager(ROIModelManager):
             with shape parameters should make use of it whenever the value
             of a shape parameter changes."""
 
-        if not self.quiet:
-            print '.....setting up diffuse backgrounds...'
-            for bg in self.bgmodels:
-                print '..........using %s'%(bg.name)
-
         nm  = len(self.models)
         rd  = self.roi_dir
 
@@ -298,7 +293,11 @@ class ROIDiffuseManager(ROIModelManager):
             band.bg_pix_counts = N.empty([len(band.wsdl),nm]) if band.has_pixels else 0
 
         # initialize models and get inital counts
+        if not self.quiet:
+            print '.....setting up diffuse/extended backgrounds for %d bands...'%len(bands)
         for ibg,bg in enumerate(self.bgmodels):
+            if not self.quiet:
+                print '.......... %s'%(bg.name) 
             bg.initialize_counts(bands)
             bg.update_counts(bands,ibg)
 
