64b3767991c7dd80419ee58696dd8ff5defc7c6a
jchiang <>
2011-03-30 00:19:57 +0000

    use suggested quadrature scheme from Jurgen and Jean in gtmodel

--- Likelihood/src/gtmodelmap/gtmodelmap.cxx
+++ Likelihood/src/gtmodelmap/gtmodelmap.cxx
@@ -300,17 +300,10 @@ void ModelMap::sumOutputMap() {
 
 double ModelMap::pixelCounts(double emin, double emax,
                              double y1, double y2) const {
-   return (y1 + y2)*(emax - emin)/2.;
-//    if (::getenv("USE_OLD_PIX_EST")) {
-//       return (y1 + y2)*(emax - emin)/2.;
-//    }
-//    double gam(std::log(y2/y1)/std::log(emax/emin));
-//    if (gam == -1) {
-//       double y0(y2/std::pow(emax, gam));
-//       return y0*std::log(emax/emin);
-//    }
-
-//    return y2/(gam + 1.)*(emax - emin*std::pow(emin/emax, gam));
+   if (::getenv("USE_LINEAR_QUADRATURE")) {
+      return (y1 + y2)*(emax - emin)/2.;
+   }
+   return (y1*emin + y2*emax)/2.*std::log(emax/emin);
 }
 
 void ModelMap::getMap(const std::string & srcName) {
