31008c89ecbba79cf76bb9a6904dfa4ff2319e92
lande <>
2011-04-04 05:14:13 +0000

    Revert buggy gradient code

--- pointlike/python/uw/like/roi_analysis.py
+++ pointlike/python/uw/like/roi_analysis.py
@@ -233,7 +233,7 @@ class ROIAnalysis(object):
             self.update_counts(parameters)
 
         # do the point sources
-        indices  = N.arange(len(models))[[N.any(m.free) for m in models]]
+        indices  = N.arange(len(models))[N.asarray([N.any(m.free) for m in models])]
         nparams  = N.asarray([model.free.sum() for model in models])
         gradient = N.zeros(nparams.sum())
 
