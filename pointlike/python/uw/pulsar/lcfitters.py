a2a8b53d75c11ff797f9a46b4569d13435a66aec
kerrm <>
2011-04-22 01:46:38 +0000

    One more stupid bug.

--- pointlike/python/uw/pulsar/lcfitters.py
+++ pointlike/python/uw/pulsar/lcfitters.py
@@ -301,7 +301,7 @@ class UnweightedLCFitter(object):
             if np.all(np.diag(c2)>0): self.cov_matrix = c2
             elif np.all(np.diag(c1)>0): self.cov_matrix = c1
             else: raise ValueError
-        except ValueError:
+        except:
             print 'Unable to invert hessian!'
             self.cov_matrix = np.zeros_like(h1)
 
