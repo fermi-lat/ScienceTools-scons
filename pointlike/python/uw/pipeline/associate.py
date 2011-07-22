20cd94baacde3904bebdbcedde7e0ce7d96e8282
burnett <>
2011-07-22 13:48:36 +0000

    Use 2FGL for associations

--- pointlike/python/uw/pipeline/associate.py
+++ pointlike/python/uw/pipeline/associate.py
@@ -29,7 +29,7 @@ class Association():
                   get default catalog association
         """
         self.acat = acat 
-        self.hdu  = pyfits.open(acat)
+        self.hdu  = pyfits.open(os.path.expandvars(acat))
         self.id_cat= self.hdu[2].data
         self.sources=self.hdu[1].data
         self.n   = len(self.sources)
@@ -59,16 +59,18 @@ class Association():
         return np.rec.fromarrays([name,cat,ang,prob,ra,dec], names=fields)
 
 
-    def __call__(self, name, sdir, ellipse): # dummy for compatibility
+    def __call__(self, name, sdir=None, ellipse=None): # dummy for compatibility
         """ return None or a dict
         """
-        if name[0]=='J': name='1FGL '+name
+        if name[0]=='J': name='2FGL '+name
         if name[-1]=='c': name=name[:-1] # drop qualifier character
-        check = self.sources.Source_Name==name
-        if check.sum()!=1:
+        for fld in ('Source_Name', 'NickName'):
+            check = self.sources.field(fld)==name
+            if check.sum()==1: break
+        if check.sum()!=1:    
             text = 'failed to find source %s in catalog %s' %( name, self.acat)
             print text
-            raise Exception(text)
+            return None #raise Exception(text)
         i = np.arange(self.n)[check][0]
         d = dict()
         r = self[i]
