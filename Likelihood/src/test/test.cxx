ac13223af2bd5fd28c33f99e0fd493d3cef98fc7
jchiang <>
2011-09-26 19:29:55 +0000

    bug-fixes for 32-bit platforms

--- Likelihood/src/test/test.cxx
+++ Likelihood/src/test/test.cxx
@@ -1305,9 +1305,9 @@ void LikelihoodTests::test_Source_Npred() {
    double dec(-5.82);
    double emin[] = {67., 1e2, 1e3, 5e3, 1e4};
    double emax[] = {1e5, 1e5, 1e5, 1e5, 1e5};
-   int nee = sizeof(emin)/sizeof(double*);
+   int nee = 5;
    for (int i(0); i < nee; i++) {
-      m_roiCuts->setCuts(193.98, -5.82, 20, emin[i], 
+      m_roiCuts->setCuts(ra, dec, 20, emin[i], 
                          emax[i], 0, 1e12, -1, true);
       SourceFactory * srcFactory = srcFactoryInstance();
       Source * src = srcFactory->create("Crab Pulsar");
