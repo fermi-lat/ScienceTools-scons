7de255a2317bf5ffe8867e77e92c835e3541374f
vlasisva <>
2011-10-03 11:55:51 +0000

    Removed ref to obsolete function makecrbkg lcurve

--- BackgroundEstimator/BackgroundEstimator/GANGSTER.h
+++ BackgroundEstimator/BackgroundEstimator/GANGSTER.h
@@ -13,7 +13,6 @@ namespace GANGSTER{
     ///Integrate the background map over the ROI
     string PlotBackground(string Interval_name, double MET, double DURATION, string FT1_FILE, string FT2_FILE, string DATACLASS, double Energy_Min_user, double Energy_Max_user, int Energy_Bins_user, float FT1ZenithTheta_Cut,  bool OverwritePlots=true, int verbosity=1, double MET_FOR_THETA=-1, bool Save_Earth_Coo_Map=false);
     int MakeGtLikeTemplate(float gtlike_ROI, string GRB_DIR, string DATACLASS, float ZTheta_Cut, double &GALGAMMAS_BKG, double &CR_EGAL_BKG, int verbosity=1);
-    int MakeCRBackground_LC(string FT1_FILE, string FT2_FILE, string DATACLASS, string TIMEDATA_FILE,bool OverwritePlots, int verbosity, float gtlike_ROI);
     ///Plot a bkg-subtracted light curve et al.
     void PlotAfterglow(double GRB_TRIGGER_TIME, string TIMEDATA_FILE, string DATACLASS, int MinBin=-1, int MaxBin=-1, float a=-2.0);
     void Calc_TimeCorrectionFactors(vector<string> GRB_folders, vector  <double> METs, vector <double> GRB_L, vector <double> GRB_B,  string Dataclass, double MinE, double MaxE, int NBins, const int Max_iE_For_Correction=25);
