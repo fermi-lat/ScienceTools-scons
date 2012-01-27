ffe1460b71073d5c51774822736271435e4208b2
burnett <>
2012-01-27 15:10:59 +0000

    change to simplify startup

--- pointlike/python/uw/like2/__init__.py
+++ pointlike/python/uw/like2/__init__.py
@@ -1,8 +1,30 @@
 """
-Package containing an alternative (to like) likelihood analysis view
+Package containing likelihood analysis
 
 $Header$
 Authors:  T. Burnett, M. Kerr, E. Wallace, M. Roth, J. Lande
 
+Modules in this package, and basic dependencies
+
+main 
+    roisetup 
+        diffuse 
+            models
+        dataset
+        skymodel
+            sources
+    roistat
+        sourcelist
+        bandlike
+    localization 
+        plotting.tsmaps
+    sedfuns
+    printing
+    
+contained packages
+    pipeline
+    plotting
+    pub
 """
 
+from .main import factory
