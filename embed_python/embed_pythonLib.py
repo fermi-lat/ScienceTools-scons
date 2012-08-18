671ebbbcc4cbaaeb356de3996ebbc9910241e54e
jrb <>
2012-08-18 00:26:21 +0000

    support no-install headers on Windows

--- embed_python/embed_pythonLib.py
+++ embed_python/embed_pythonLib.py
@@ -4,7 +4,10 @@ def generate(env, **kw):
         env.Tool('addLibrary', library = ['embed_python'])
 	if env['PLATFORM'] == 'posix':
             env.AppendUnique(LINKFLAGS	= ['-rdynamic'])	
+        if env['PLATFORM'] == "win32" and env.get('CONTAINERNAME','') == 'GlastRelease':
+            env.Tool('findPkgPath', package = 'embed_python') 
     env.Tool('addLibrary', library = env['pythonLibs'])
+    # No need for incsOnly section
 
 def exists(env):
     return 1
--- fitsGen/fitsGenLib.py
+++ fitsGen/fitsGenLib.py
@@ -2,12 +2,24 @@
 def generate(env, **kw):
     if not kw.get('depsOnly',0):
         env.Tool('addLibrary', library = ['fitsGen'])
+	if env['PLATFORM'] == 'win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'fitsGen') 
+	    env.Tool('findPkgPath', package = 'embed_python') 
+            env.Tool('findPkgPath', package = 'tip') 
+            env.Tool('findPkgPath', package = 'astro') 
+            env.Tool('findPkgPath', package = 'facilities') 
     env.Tool('facilitiesLib')
     env.Tool('tipLib')
     env.Tool('astroLib')
     env.Tool('embed_pythonLib')
     env.Tool('evtUtilsLib')
     env.Tool('addLibrary', library = env['rootGuiLibs'])
+    if kw.get('incsOnly', 0) == 1: 
+        env.Tool('findPkgPath', package = 'embed_python') 
+        env.Tool('findPkgPath', package = 'tip') 
+        env.Tool('findPkgPath', package = 'astro') 
+        env.Tool('findPkgPath', package = 'facilities') 
+        env.Tool('findPkgPath', package = 'evtUtils') 
 
 def exists(env):
     return 1
