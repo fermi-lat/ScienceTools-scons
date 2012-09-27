8957779a8caa15246caf37309a517ad9e8e0307e
lande <>
2012-09-27 21:36:51 +0000

    better doctests

--- pointlike/python/uw/darkmatter/spatial.py
+++ pointlike/python/uw/darkmatter/spatial.py
@@ -29,6 +29,19 @@ class NFW(InterpProfile2D):
            >>> profile = NFW(sigma=sigma)
            >>> print profile.at_r_in_deg(r)
            [  3.46679557e+01   2.73145672e+00   1.51607416e-01   6.68125546e-03]
+
+        Note, previously this wouldn't raise an exception:
+            >>> np.allclose(profile.default_limits[-1],[0.01, 15])
+            True
+            >>> profile['sigma'] = 0.009
+            Traceback (most recent call last):
+                ...
+            SpatialModelException: sigma=0.009 is lower than the lower limit=0.01:
+            >>> profile['sigma'] = 15.001
+            Traceback (most recent call last):
+                ...
+            SpatialModelException: sigma=15.001 is larger than the upper limit=15.0:
+
         """
 
         infile = path.expand(file)
