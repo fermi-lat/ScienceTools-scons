0852ee8f5f0fd570c91d4ebef8308c3f8a815812
burnett <>
2012-11-18 20:03:31 +0000

    bug fix

--- pointlike/python/uw/like2/catrec.py
+++ pointlike/python/uw/like2/catrec.py
@@ -63,7 +63,7 @@ def create_catalog(outdir, **kwargs):
     addtorec = kwargs.pop('addtorec', None)
     assert len(kwargs.keys())==0, 'unrecognized kw %s' %kwargs 
     if 'LATEXTDIR' not in os.environ:
-        t = os.path.join(os.environ['FERMI'],'catalog','Extended_archive_v10') 
+        t = os.path.join(os.environ['FERMI'],'catalog','Extended_archive_v12') 
         assert os.path.exists(os.path.join(t,'Templates')), 'path %s not found' %t
         os.environ['LATEXTDIR']=t
     
