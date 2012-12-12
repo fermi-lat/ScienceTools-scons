ff5b65790d4043214385e3f0750fe0c27e2fe8de
cohen <>
2012-12-12 10:01:51 +0000

    try to fix windows build?

--- st_facilities/st_facilitiesLib.py
+++ st_facilities/st_facilitiesLib.py
@@ -1,15 +1,13 @@
 #$Id$
 def generate(env, **kw):
-	if not kw.get('depsOnly',0):
-	    env.Tool('addLibrary', library = ['st_facilities'])
-
-	    if env['PLATFORM'] == 'win32'and env.get('CONTAINERNAME','')=='GlastRelease':
-		env.Tool('findPkgPath', package = 'st_facilities') 
-
-
-	env.Tool('astroLib')
-	env.Tool('addLibrary', library = env['cfitsioLibs'])
-	env.Tool('addLibrary', library = env['f2cLibs'])
+    if not kw.get('depsOnly',0):
+        env.Tool('addLibrary', library = ['st_facilities'])
+	if env['PLATFORM'] == 'win32'and env.get('CONTAINERNAME','')=='GlastRelease':
+            env.Tool('findPkgPath', package = 'st_facilities') 
+    env.Tool('astroLib')
+    env.Tool('addLibrary', library = env['cfitsioLibs'])
+    env.Tool('addLibrary', library = env['f2cLibs'])
+    env.Tool('addLibrary', library = env['cppunitLibs'])
 
 
 def exists(env):
