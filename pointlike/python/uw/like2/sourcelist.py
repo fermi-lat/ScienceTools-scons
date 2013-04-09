145111e74cebd82472506fc8f6fea95942356441
burnett <>
2013-04-09 21:26:28 +0000

    change limits for prefactor

--- pointlike/python/uw/like2/sourcelist.py
+++ pointlike/python/uw/like2/sourcelist.py
@@ -44,7 +44,7 @@ def set_default_bounds( model, force=False):
         try:
             plim = dict(
                 Index=(-0.5, 5), 
-                Norm=(10**-15, 10**-7),
+                Norm=(10**-16, 10**-7),
                 Scale=(0.001, 4.0),
                 beta=(-0.1, 5.), 
                 Cutoff=(100., 1e5),
@@ -259,7 +259,7 @@ class SourceList(list):
             try:
                 plim = dict(
                     Index=(-0.5, 5), 
-                    Norm=(10**-15, 10**-7),
+                    Norm=(10**-16, 10**-7),
                     Scale=(0.0001, 10.0),
                     beta=(-0.1, 5.), 
                     Cutoff=(100., 1e5),
