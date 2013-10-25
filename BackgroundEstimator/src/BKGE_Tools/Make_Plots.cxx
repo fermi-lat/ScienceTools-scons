56897d5ac3381bacc35c23044d024c886290cf93
vlasisva <>
2013-10-25 09:57:54 +0000

    Improved handling of changes in Fermi FT2 files definition Cleaned up code Stopped saving and calculating some unnecessary distributions (LAT, LON, McIlwainB)

--- BackgroundEstimator/src/BKGE_Tools/Make_Plots.cxx
+++ BackgroundEstimator/src/BKGE_Tools/Make_Plots.cxx
@@ -1,9 +1,8 @@
 //Author: Vlasios Vasileiou <vlasisva@gmail.com>
-// $Header$
 #include "BackgroundEstimator/BKGE_Tools.h"
 #include "TGraph.h"
 
-void Interpolate(TH1F * hist, bool aRA=false);
+void Interpolate(TH1F * hist, bool aRA=false, int verbosity=0);
 void CalculateRockingAngle(TH1F* hRockingAnglevsTime, TH1F* hPtRazvsTime, TH1F* hPtDeczvsTime,  TH1F* hPtRaxvsTime, TH1F* hPtDecxvsTime, TH1F* hRAZenithvsTime, TH1F* hDecZenithvsTime);
 
 int TOOLS::Make_Plots(double PreTime, double PostTime, double GRB_t0, string PlotsFile, string FT2_FILE, int verbosity, float TimeStep){
@@ -40,13 +39,23 @@ int TOOLS::Make_Plots(double PreTime, double PostTime, double GRB_t0, string Plo
  fits_open_file(&fptr, FT2_FILE.c_str(), READONLY, &status);
  fits_movabs_hdu(fptr, 2, &hdutype, &status);
  fits_get_num_rows(fptr, &nrows, &status);
- fits_read_col (fptr,TDOUBLE,1,1, 1, 1, NULL,&FT2_START, &anynul, &status);
- fits_read_col (fptr,TDOUBLE,2,nrows, 1, 1, NULL,&FT2_END, &anynul, &status);
+ int col_start; fits_get_colnum(fptr, TRUE, "START", &col_start, &status);
+ int col_stop; fits_get_colnum(fptr, TRUE, "STOP", &col_stop, &status);
+ int col_razen;  fits_get_colnum(fptr, TRUE, "RA_ZENITH", &col_razen, &status);
+ int col_deczen;  fits_get_colnum(fptr, TRUE, "DEC_ZENITH", &col_deczen, &status);
+ int col_mcilwainl; fits_get_colnum(fptr, TRUE, "L_MCILWAIN", &col_mcilwainl, &status);
+ int col_rascz; fits_get_colnum(fptr, TRUE, "RA_SCZ", &col_rascz, &status);
+ int col_decscz; fits_get_colnum(fptr, TRUE, "DEC_SCZ", &col_decscz, &status);
+ int col_rascx; fits_get_colnum(fptr, TRUE, "RA_SCX", &col_rascx, &status);
+ int col_decscx; fits_get_colnum(fptr, TRUE, "DEC_SCX", &col_decscx, &status);
+
+ fits_read_col (fptr,TDOUBLE,col_start,1, 1, 1, NULL,&FT2_START, &anynul, &status);
+ fits_read_col (fptr,TDOUBLE,col_stop,nrows, 1, 1, NULL,&FT2_END, &anynul, &status);
 
  //decide if we have FT2 or FT2Seconds file
  double a1,a2;
- fits_read_col (fptr,TDOUBLE,1,1, 1, 1, NULL,&a1, &anynul, &status);
- fits_read_col (fptr,TDOUBLE,2,1, 1, 1, NULL,&a2, &anynul, &status);
+ fits_read_col (fptr,TDOUBLE,col_start,1, 1, 1, NULL,&a1, &anynul, &status);
+ fits_read_col (fptr,TDOUBLE,col_stop,1, 1, 1, NULL,&a2, &anynul, &status);
  int TimeExtension=0; //in sec
  bool FT2SECONDS=true;
 
@@ -68,50 +77,49 @@ int TOOLS::Make_Plots(double PreTime, double PostTime, double GRB_t0, string Plo
  TH1F hPtDeczvsTime = TH1F("hPtDeczvsTime","PtDecz vs Time", TimeBins,StartTime,StopTime);
  TH1F hPtDecxvsTime = TH1F("hPtDecxvsTime","PtDecx vs Time", TimeBins,StartTime,StopTime);
  TH1F hMcIlwainLvsTime = TH1F("hMcIlwainLvsTime","McIlwainLvsTime",TimeBins,StartTime,StopTime);
- TH1F hMcIlwainBvsTime = TH1F("hMcIlwainBvsTime","McIlwainBvsTime",TimeBins,StartTime,StopTime);
  TH1F hRAZenithvsTime = TH1F("hRAZenithvsTime","RAZenithvsTime",TimeBins,StartTime,StopTime);
  TH1F hDecZenithvsTime = TH1F("hDecZenithvsTime","DecZenithvsTime",TimeBins,StartTime,StopTime);
- TH1F hLatitudevsTime = TH1F("hLatitudevsTime","Latitude",TimeBins,StartTime,StopTime);
- TH1F hLongitudevsTime = TH1F("hLongitudevsTime","Longitude",TimeBins,StartTime,StopTime);
 
  
  if (FT2_START>hRAZenithvsTime.GetXaxis()->GetXmin()) {
   printf("%s: FT2 file starts at time %f and we need data from an earlier time %f to make the plots\n",
        __FUNCTION__,FT2_START,StartTime);
-  return -1;
+  exit(1);
  } 
 
- fits_read_col (fptr,TDOUBLE,1,nrows, 1, 1, NULL,&FT2_END, &anynul, &status);
+ fits_read_col (fptr,TDOUBLE,col_stop,nrows, 1, 1, NULL,&FT2_END, &anynul, &status);
  if (FT2_END<hRAZenithvsTime.GetXaxis()->GetXmax()) {
       printf("%s: FT2 file stops at time %f and we need data up to a later time %f to make the plots\n",
            __FUNCTION__,FT2_END,StopTime);
-     return -1;
+     exit(1);
  } 
 
  int NBins=hRAZenithvsTime.GetNbinsX();
- double START,RA_ZENITH,DEC_ZENITH,PTRAZ,PTRAX,PTDECZ,PTDECX,MCILWAINL,STOP,MCILWAINB,LATITUDE,LONGITUDE;
-
+ double START,RA_ZENITH,DEC_ZENITH,PTRAZ,PTRAX,PTDECZ,PTDECX,MCILWAINL,STOP;
+ 
  for (long jj = 1; jj < nrows && !status; jj++) {
+     int col_num;
      //if (jj%100==0){ printf("%5.2f\r",jj/float(nrows)); fflush(0);}
-     fits_read_col (fptr,TDOUBLE,1,jj, 1, 1, NULL,&START, &anynul, &status);
-     fits_read_col (fptr,TDOUBLE,2,jj, 1, 1, NULL,&STOP, &anynul, &status);
-     int ibin = hRAZenithvsTime.FindBin(START);
-     int ibinSTOP = hRAZenithvsTime.FindBin(STOP);
+
+     fits_read_col (fptr,TDOUBLE,col_start,jj, 1, 1, NULL,&START, &anynul, &status);
+     if (status) fits_report_error(stderr, status); 
+     fits_read_col (fptr,TDOUBLE,col_stop,jj, 1, 1, NULL,&STOP, &anynul, &status);
+     if (status) fits_report_error(stderr, status);
+     long int ibin = hRAZenithvsTime.FindBin(START);
+     long int ibinSTOP = hRAZenithvsTime.FindBin(STOP);
 
      if (ibinSTOP<1) continue;
      if (ibin>NBins) break;
+    //Format of FT2 files seems to change all the time so let's try to be cautious...    
 
-    fits_read_col (fptr,TDOUBLE,4,jj, 1, 1, NULL,&LATITUDE, &anynul, &status);
-    fits_read_col (fptr,TDOUBLE,5,jj, 1, 1, NULL,&LONGITUDE, &anynul, &status);
-    fits_read_col (fptr,TDOUBLE,7,jj, 1, 1, NULL,&RA_ZENITH, &anynul, &status);
-    fits_read_col (fptr,TDOUBLE,8,jj, 1, 1, NULL,&DEC_ZENITH, &anynul, &status);
-    fits_read_col (fptr,TDOUBLE,10,jj, 1, 1, NULL,&MCILWAINL, &anynul, &status);
-    fits_read_col (fptr,TDOUBLE,9,jj, 1, 1, NULL,&MCILWAINB, &anynul, &status);
-    fits_read_col (fptr,TDOUBLE,13,jj, 1, 1, NULL,&PTRAZ, &anynul, &status);
-    fits_read_col (fptr,TDOUBLE,14,jj, 1, 1, NULL,&PTDECZ, &anynul, &status);
-    fits_read_col (fptr,TDOUBLE,15,jj, 1, 1, NULL,&PTRAX, &anynul, &status);
-    fits_read_col (fptr,TDOUBLE,16,jj, 1, 1, NULL,&PTDECX, &anynul, &status);
-    LONGITUDE+=180;
+    fits_read_col (fptr,TDOUBLE,col_razen,jj, 1, 1, NULL,&RA_ZENITH, &anynul, &status);
+    fits_read_col (fptr,TDOUBLE,col_deczen,jj, 1, 1, NULL,&DEC_ZENITH, &anynul, &status);
+    fits_read_col (fptr,TDOUBLE,col_mcilwainl,jj, 1, 1, NULL,&MCILWAINL, &anynul, &status);
+    fits_read_col (fptr,TDOUBLE,col_rascz,jj, 1, 1, NULL,&PTRAZ, &anynul, &status);
+    fits_read_col (fptr,TDOUBLE,col_decscz,jj, 1, 1, NULL,&PTDECZ, &anynul, &status);
+    fits_read_col (fptr,TDOUBLE,col_rascx,jj, 1, 1, NULL,&PTRAX, &anynul, &status);
+    fits_read_col (fptr,TDOUBLE,col_decscx,jj, 1, 1, NULL,&PTDECX, &anynul, &status);    
+
     hPtRazvsTime.SetBinContent(ibin,PTRAZ);
     hPtRaxvsTime.SetBinContent(ibin,PTRAX);
     hPtDeczvsTime.SetBinContent(ibin,PTDECZ);
@@ -119,14 +127,9 @@ int TOOLS::Make_Plots(double PreTime, double PostTime, double GRB_t0, string Plo
     hRAZenithvsTime.SetBinContent(ibin,RA_ZENITH);
     hDecZenithvsTime.SetBinContent(ibin,DEC_ZENITH);
     hMcIlwainLvsTime.SetBinContent(ibin,MCILWAINL);
-    hMcIlwainBvsTime.SetBinContent(ibin,MCILWAINB);
-    hLatitudevsTime.SetBinContent(ibin,LATITUDE);
-    hLongitudevsTime.SetBinContent(ibin,LONGITUDE);
     //Fill both START and STOP. If START of next event 
     //printf("ibin=%d ibinistop=%d\n",ibin,ibinSTOP);
     if (ibinSTOP!=ibin) {
-      hLatitudevsTime.SetBinContent(ibinSTOP,LATITUDE);
-      hLongitudevsTime.SetBinContent(ibinSTOP,LONGITUDE);
       hPtRazvsTime.SetBinContent(ibinSTOP,PTRAZ);
       hPtRaxvsTime.SetBinContent(ibinSTOP,PTRAX);
       hPtDeczvsTime.SetBinContent(ibinSTOP,PTDECZ);
@@ -134,11 +137,8 @@ int TOOLS::Make_Plots(double PreTime, double PostTime, double GRB_t0, string Plo
       hRAZenithvsTime.SetBinContent(ibinSTOP,RA_ZENITH);
       hDecZenithvsTime.SetBinContent(ibinSTOP,DEC_ZENITH);
       hMcIlwainLvsTime.SetBinContent(ibinSTOP,MCILWAINL);
-      hMcIlwainBvsTime.SetBinContent(ibinSTOP,MCILWAINB);
     }
     if ((ibinSTOP-ibin)==2 && FT2SECONDS ) { //rounding errors fix
-      hLatitudevsTime.SetBinContent(ibinSTOP-1,LATITUDE);
-      hLongitudevsTime.SetBinContent(ibinSTOP-1,LONGITUDE);
       hPtRazvsTime.SetBinContent(ibinSTOP-1,PTRAZ);
       hPtRaxvsTime.SetBinContent(ibinSTOP-1,PTRAX);
       hPtDeczvsTime.SetBinContent(ibinSTOP-1,PTDECZ);
@@ -146,10 +146,8 @@ int TOOLS::Make_Plots(double PreTime, double PostTime, double GRB_t0, string Plo
       hRAZenithvsTime.SetBinContent(ibinSTOP-1,RA_ZENITH);
       hDecZenithvsTime.SetBinContent(ibinSTOP-1,DEC_ZENITH);
       hMcIlwainLvsTime.SetBinContent(ibinSTOP-1,MCILWAINL);
-      hMcIlwainBvsTime.SetBinContent(ibinSTOP-1,MCILWAINB);
     }
-
-
+    if (status) fits_report_error(stderr, status);
     //printf("%d %d %lf %lf %lf %lf j=%ld stat=%d %lf\n",ibin,ibinSTOP,START,STOP,hPtRazvsTime.GetXaxis()->GetBinLowEdge(3),hPtRazvsTime.GetXaxis()->GetBinUpEdge(3),jj,status,PTRAZ);
 
  }
@@ -159,38 +157,29 @@ int TOOLS::Make_Plots(double PreTime, double PostTime, double GRB_t0, string Plo
 
  if (!FT2SECONDS) {
    hPtRazvsTime.Write("hPtRazvsTime_uninterpolated");
-   hLatitudevsTime.Write("hLatitudevsTime_uninterpolated");
-   hLongitudevsTime.Write("hLongitudevsTime_uninterpolated");
    hPtRaxvsTime.Write("hPtRaxvsTime_uninterpolated");
    hPtDeczvsTime.Write("hPtDeczvsTime_uninterpolated");
    hPtDecxvsTime.Write("hPtDecxvsTime_uninterpolated");
    hRAZenithvsTime.Write("hRAZenithvsTime_uninterpolated");
    hDecZenithvsTime.Write("hDecZenithvsTime_uninterpolated");
    hMcIlwainLvsTime.Write("hMcIlwainLvsTime_uninterpolated");
-   hMcIlwainBvsTime.Write("hMcIlwainBvsTime_uninterpolated");
 
-   Interpolate(&hPtRazvsTime,true);
-   Interpolate(&hLatitudevsTime);
-   Interpolate(&hLongitudevsTime,true);
-   Interpolate(&hPtRaxvsTime,true);
-   Interpolate(&hPtDeczvsTime);
-   Interpolate(&hPtDecxvsTime);
-   Interpolate(&hRAZenithvsTime,true);
-   Interpolate(&hDecZenithvsTime);
-   Interpolate(&hMcIlwainLvsTime);
-   Interpolate(&hMcIlwainBvsTime);
+   Interpolate(&hPtRazvsTime,true,verbosity);
+   Interpolate(&hPtRaxvsTime,true,verbosity);
+   Interpolate(&hPtDeczvsTime,false,verbosity);
+   Interpolate(&hPtDecxvsTime,false,verbosity);
+   Interpolate(&hRAZenithvsTime,true,verbosity);
+   Interpolate(&hDecZenithvsTime,false,verbosity);
+   Interpolate(&hMcIlwainLvsTime,false,verbosity);
 
 
    hPtRazvsTime.Write("hPtRazvsTime_interpolated");
-   hLatitudevsTime.Write("hLatitudeTime_interpolated");
-   hLongitudevsTime.Write("hLongitudevsTime_interpolated");
    hPtRaxvsTime.Write("hPtRaxvsTime_interpolated");
    hPtDeczvsTime.Write("hPtDeczvsTime_interpolated");
    hPtDecxvsTime.Write("hPtDecxvsTime_interpolated");
    hRAZenithvsTime.Write("hRAZenithvsTime_interpolated");
    hDecZenithvsTime.Write("hDecZenithvsTime_interpolated");
    hMcIlwainLvsTime.Write("hMcIlwainLvsTime_interpolated");
-   hMcIlwainBvsTime.Write("hMcIlwainBvsTime_interpolated");
  
 
    TH1F _hPtRazvsTime = TH1F("hPtRazvsTime","PtRaz vs Time", TimeBins_orig,StartTime_orig,StopTime_orig);
@@ -198,11 +187,8 @@ int TOOLS::Make_Plots(double PreTime, double PostTime, double GRB_t0, string Plo
    TH1F _hPtDeczvsTime = TH1F("hPtDeczvsTime","PtDecz vs Time", TimeBins_orig,StartTime_orig,StopTime_orig);
    TH1F _hPtDecxvsTime = TH1F("hPtDecxvsTime","PtDecx vs Time", TimeBins_orig,StartTime_orig,StopTime_orig);
    TH1F _hMcIlwainLvsTime = TH1F("hMcIlwainLvsTime","McIlwainLvsTime",TimeBins_orig,StartTime_orig,StopTime_orig);
-   TH1F _hMcIlwainBvsTime = TH1F("hMcIlwainBvsTime","McIlwainBvsTime",TimeBins_orig,StartTime_orig,StopTime_orig);
    TH1F _hRAZenithvsTime = TH1F("hRAZenithvsTime","RAZenithvsTime",TimeBins_orig,StartTime_orig,StopTime_orig);
    TH1F _hDecZenithvsTime = TH1F("hDecZenithvsTime","DecZenithvsTime",TimeBins_orig,StartTime_orig,StopTime_orig);
-   TH1F _hLatitudevsTime = TH1F("hLatitudevsTime","LatitudevsTime",TimeBins_orig,StartTime_orig,StopTime_orig);
-   TH1F _hLongitudevsTime = TH1F("hLongitudevsTime","LongitudevsTime",TimeBins_orig,StartTime_orig,StopTime_orig);
    TH1F _hRockingAnglevsTime  = TH1F("hRockingAnglevsTime","RockingAnglevsTime",TimeBins_orig,StartTime_orig,StopTime_orig);
 
    int ib;
@@ -214,18 +200,12 @@ int TOOLS::Make_Plots(double PreTime, double PostTime, double GRB_t0, string Plo
        _hPtRaxvsTime.SetBinContent(i,hPtRaxvsTime.GetBinContent(ib));
        _hPtDeczvsTime.SetBinContent(i,hPtDeczvsTime.GetBinContent(ib));
        _hPtDecxvsTime.SetBinContent(i,hPtDecxvsTime.GetBinContent(ib));
-       _hLatitudevsTime.SetBinContent(i,hLatitudevsTime.GetBinContent(ib));
-       _hLongitudevsTime.SetBinContent(i,hLongitudevsTime.GetBinContent(ib));
        _hRAZenithvsTime.SetBinContent(i,hRAZenithvsTime.GetBinContent(ib));
        _hDecZenithvsTime.SetBinContent(i,hDecZenithvsTime.GetBinContent(ib));
        _hMcIlwainLvsTime.SetBinContent(i,hMcIlwainLvsTime.GetBinContent(ib));
-       _hMcIlwainBvsTime.SetBinContent(i,hMcIlwainBvsTime.GetBinContent(ib));
    }
 
    _hMcIlwainLvsTime.Write();
-   _hLatitudevsTime.Write();
-   _hLongitudevsTime.Write();
-   _hMcIlwainBvsTime.Write();
    _hDecZenithvsTime.Write();
    _hRAZenithvsTime.Write();
    _hPtDecxvsTime.Write();
@@ -239,9 +219,6 @@ int TOOLS::Make_Plots(double PreTime, double PostTime, double GRB_t0, string Plo
    TH1F hRockingAnglevsTime  = TH1F("hRockingAnglevsTime","RockingAnglevsTime",TimeBins,StartTime,StopTime);
    CalculateRockingAngle(&hRockingAnglevsTime,&hPtRazvsTime,&hPtDeczvsTime, &hPtRaxvsTime, &hPtDecxvsTime, &hRAZenithvsTime, &hDecZenithvsTime);
    hMcIlwainLvsTime.Write();
-   hLatitudevsTime.Write();
-   hLongitudevsTime.Write();
-   hMcIlwainBvsTime.Write();
    hDecZenithvsTime.Write();
    hRAZenithvsTime.Write();
    hPtDecxvsTime.Write();
@@ -258,10 +235,9 @@ int TOOLS::Make_Plots(double PreTime, double PostTime, double GRB_t0, string Plo
 
 //function that interpolates pointing plots
 //takes care of 360.0 and 0.360 crossings of RAs
-void Interpolate(TH1F* hist,bool aRA) {
- #ifdef DEBUG
- printf("Interpolate %s\n",hist.GetTitle());
- #endif
+void Interpolate(TH1F* hist,bool aRA,int verbosity) {
+ //if (verbosity>1) printf("Interpolate %s\n",hist->GetTitle());
+ 
  double X[200],Y[200];
  float aval;
  int ilast_filled=1;//last filled i
@@ -270,7 +246,7 @@ void Interpolate(TH1F* hist,bool aRA) {
  TGraph g;
 
  for (int i=1;i<=hist->GetNbinsX();i++) {
-    //printf("%d\%d \r",i,hist->GetNbinsX()); fflush(0);
+    if (verbosity>1 && i%1000==0) {printf("%.2f \r",i/float(hist->GetNbinsX())); fflush(0);}
     if ((ilast_filled && (i-ilast_filled)>33 && nel<=1))  {  //there is a gap, reset table
         //  printf("long gap nel=%d i,ilast=%d %d %d time=%f %f\n",nel,i,ilast_filled,i-ilast_filled,hist->GetBinCenter(i),hist->GetBinCenter(ilast_filled));
          ilast_filled=0;
