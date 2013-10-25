870f7d3c5d5b7674c2102fda8fef76881a3c629c
vlasisva <>
2013-10-25 10:33:53 +0000

    Update following the developments of the BKGE paper

--- BackgroundEstimator/src/BackgroundEstimator/DecomposeError.cxx
+++ BackgroundEstimator/src/BackgroundEstimator/DecomposeError.cxx
@@ -1,230 +1,241 @@
-/*
-double WeightedP(int nev,double bkg,double dbkg);
-double ProbToSigma(double Prob);
+#include "BackgroundEstimator/BackgroundEstimator.h"
+#include "TLegend.h"
+#include "TGraph.h"
+#include "Math/ProbFuncMathCore.h"
+#include "Math/PdfFuncMathCore.h"
+#include "Math/QuantFuncMathCore.h"
+#include <algorithm>
+#include "TLine.h"
 
-double DecomposeError(int ent0, double Back0[], double MeasSig0[], bool Plot){
-Plot=false;
- gSystem->Load("libMathCore");
+double WeightedP(int nev, float bkg, float dbkg);
+
+
+float  BKGE_NS::DecomposeError(int ent0, float Back0[], double MeasSig0[], bool Plot){
+
+ 
  const int bins=100;
  const int binss=100;
 
- double MeasSig[4000],Back[4000];
- int ent=0;
+ vector <double> MeasSig;
+ vector <double> Back;
 
+ //select for analysis only cases that the expected bkg >5 events
  for (int i=0;i<ent0;i++) {
     if (Back0[i]>20){
-       MeasSig[ent]=MeasSig0[i];
-       Back[ent]=Back0[i];
-       ent++;       
+       MeasSig.push_back(MeasSig0[i]);
+       Back.push_back(Back0[i]); 
     }
  }
+ unsigned int nData=Back.size();
 
- if (ent<10) {printf ("return\n"); return 1;}
+ if (nData<10) {printf ("%s: not enough data points.. returning\n",__FUNCTION__); return 0;}
 
- int max=-1, min=999;
- for (int i=0;i<ent;i++) {
-   if (MeasSig[i]>max) {max=MeasSig[i];}
-   if (MeasSig[i]<min) {min=MeasSig[i];}
- }
- printf("ent=%d min/max= %d/%d ,",ent,min,max);
+ const int MaxSig=TMath::MaxElement(nData,&MeasSig.front());
+ const int MinSig=TMath::MinElement(nData,&MeasSig.front());
+ printf("nData=%d MinSig/MaxSig= %d/%d ,",nData,MinSig,MaxSig);
 
-
- TH1F * hBack, * hSig, * hSigma,* hProbFlat, * hProbC,
-      * hProbCG,*hProb;
-
- TCanvas * cc_cc = NULL;
+ TCanvas * c = 0;
+ TH1F * hBack=0, * hSig=0, * hSigma=0, *hProb=0;
+ TGraph *gQ_fit=0;
+ TF1 * fitgaus= 0;
+ TGraph * gQ = 0;
+ TGraph * gCum=0;
+ TGraph * gProbvsBack=0;
  if (Plot) {
-    cc_cc= new TCanvas("cD123","cD123",1024,768);
-    cc_cc->Divide(2,3);
- }
+   c= new TCanvas("cDecompose","cDecompose",1024,768);
+   c->Divide(2,3);
 
- hProb = new TH1F("hProb","Probability distribution",binss,0,1);
- hProb->GetXaxis()->SetTitle("Probability");
- hProbFlat = new TH1F("hProbFlat","hProbFlat",binss,0,1);
- if (Plot) {
-   hBack = new TH1F("hBack","Background Estimates",bins,min,max);
+   c->cd(2); 
+   hProb = new TH1F("hProb","Probability distribution",binss,0,1);
+   hProb->GetXaxis()->SetTitle("Probability"); 
+   hProb->SetMinimum(0);
+   hProb->Draw();
+  
+   c->cd(1); 
+   hBack = new TH1F("hBack","Background Estimates",bins,MinSig,MaxSig);
    hBack->GetXaxis()->SetTitle("Estimated number of bkg events");
-   hSig = new TH1F("hSig","Signal distributions",bins,min,max);
+   hSig = new TH1F("hSig","Signal distributions",bins,MinSig,MaxSig);
    hSig->GetXaxis()->SetTitle("Number of signal events");
-   hSigma = new TH1F("hSigma","Significance distribution",100,-10,10);
-   hSigma->GetXaxis()->SetTitle("Significance (sigma)");
-   hSigma->GetYaxis()->SetTitle("dN/dP");
-   hProbC = new TH1F("hProbC","hProbC",binss,0,1);
-   hProbC->GetXaxis()->SetTitle("Probability P'");
-   hProbC->GetXaxis()->SetTitle("Number of events with P<P'");
-   hProbCG = new TH1F("hProbCG","hProbCG",binss,0,1);
-  }
-
- //Simulate
- for (int i=0;i<ent;i++) {
-     if (MeasSig[i] && Plot) {
+   for (unsigned int i=0;i<nData;i++) {
        hSig->Fill(MeasSig[i]);
        hBack->Fill(Back[i]);
-     }
+   }  
+  
+   c->GetPad(1)->SetLogy();
+   hSig->Draw(); 
+   hBack->SetLineColor(3);
+   hBack->Draw("SAME"); 
+
+   TLegend * l = new TLegend(0.7,0.8,0.9,0.9);
+   l->AddEntry(hSig,"Measured signal","l");
+   l->AddEntry(hBack,"Background","l");
+   l->Draw();
+   
+   c->cd(4);   
+   hSigma = new TH1F("hSigma","Significance distribution",30,-5,5);
+   hSigma->GetXaxis()->SetTitle("Significance (sigma)");
+   hSigma->GetYaxis()->SetTitle("dN/dP");
+      
+   c->cd(3);
+      
+   fitgaus=new TF1("fit","gaus");
+   fitgaus->SetParameters(1,0,1);
+   fitgaus->FixParameter(1,0);
+   fitgaus->FixParameter(2,1);
+
+    c->cd(5);
+    gQ = new TGraph();
+    gQ->SetTitle("Kolmogorov-test result");
+    gQ->GetXaxis()->SetTitle("Test systematic error on the bkg estimate");
+    gQ->GetYaxis()->SetTitle("Kolmogorov-test result");
+    gQ->Draw("A*");      
+    gQ->SetMinimum(0);
+ /*
+    gQ_fit = new TGraph();
+    gQ_fit->SetTitle("Kolmogorov-test result");
+    gQ_fit->GetXaxis()->SetTitle("Test systematic error on the bkg estimate");
+    gQ_fit->GetYaxis()->SetTitle("Gaussian Fit result");
+    gQ_fit->SetMarkerColor(2);
+    gQ_fit->Draw("*SAME");      
+*/
  }
 
- TGraph * gQ;
- double ERROR[100],Q[100];
- int nerrors=0;
+ short nerrors=0;
  float BestQ=0,BestProb=0;
- for (double anerror=1e-5;anerror<0.2;anerror+=0.01) {
-    hProb->Reset();
-    if (Plot) hSigma->Reset();
-    double Sigma;
-    for (int i=0;i<ent;i++) {
-        Sigma=anerror*Back[i];
-        double CorrectedIntP=0;
-      
-        int iback;
-        iback=int(Back[i]+0.5);
-        if (MeasSig[i]<=iback)for (int isig=MeasSig[i];isig<iback;isig++)       CorrectedIntP+=WeightedP(isig,Back[i],Sigma);
-        else                  for (int isig=iback+1;isig<=MeasSig[i];isig++)    CorrectedIntP+=WeightedP(isig,Back[i],Sigma);
-        CorrectedIntP*=2;
-        CorrectedIntP+=WeightedP(iback,iback,Sigma);
+ const int nData_null=1000;
+ vector <double> CorrectedPValues_null(nData_null);
+ for (int i=0;i<nData_null;i++) CorrectedPValues_null[i]=gRandom->Uniform();
+ sort(CorrectedPValues_null.begin(),CorrectedPValues_null.end()); 
+ 
+ for (float anerror=0;anerror<0.2;anerror+=0.01) { //loop over systematic error on the bkg
    
-        if (CorrectedIntP>1) CorrectedIntP=0.99;
-        if (CorrectedIntP<=0) {
-           printf("corp=%e MeasSig[i]=%.1f  back=%f iback=%d\n",CorrectedIntP,MeasSig[i],Back[i],iback); //check to see if denominator is ~1 as it should be
-           CorrectedIntP=1e-1;     
-        }
+    if (Plot) { hProb->Reset();hSigma->Reset();}
+   
+    vector <double> CorrectedPValues(nData);
+   
+    for (unsigned int i=0;i<nData;i++) { //process entries
+        float aBkgError=anerror*Back[i];
+       
+      
+        /*
+        int iback=floor(Back[i]+0.5);
+        if (MeasSig[i]<=iback) for (int isig=MeasSig[i];isig<iback;isig++)       CorrectedIntP+=WeightedP(isig,Back[i],aBkgError);
+        else                   for (int isig=iback+1;isig<=MeasSig[i];isig++)    CorrectedIntP+=WeightedP(isig,Back[i],aBkgError);
+        CorrectedIntP*=2;
+        CorrectedIntP+=WeightedP(iback,iback,aBkgError);
+        */
 
-        CorrectedIntP=1-CorrectedIntP;
-        hProb->Fill(CorrectedIntP);  
+        if (anerror<1e-4) {
+           CorrectedPValues[i]=ROOT::Math::poisson_cdf_c(MeasSig[i],Back[i]);//ROOT::Math::poisson_pdf(MeasSig[i],Back[i])/2.;
+          //CorrectedPValues[i]=ROOT::Math::poisson_cdf_c(MeasSig[i],Back[i]);//-ROOT::Math::poisson_pdf(MeasSig[i],Back[i]);
+        }   
+        else {
+          for (int asig=0;asig<MeasSig[i];asig++) CorrectedPValues[i]+=WeightedP(asig,Back[i],aBkgError);
+          //CorrectedPValues[i]+=WeightedP(MeasSig[i],Back[i],aBkgError)/2.;    
+          //CorrectedPValues[i]+=WeightedP(MeasSig[i],Back[i],aBkgError);
+        
+          if (CorrectedPValues[i]>1.000001) {
+            printf("correctedP>1? p=%f sig=%f bk=%f\n",CorrectedPValues[i],MeasSig[i],Back[i]);
+            CorrectedPValues[i]=0.9999;
+          }    
+          if (CorrectedPValues[i]<=0) {
+             printf("correctedP<0?\n");
+             //printf("corp=%e MeasSig[i]=%.1f  back=%f iback=%d\n",CorrectedPValues[i],MeasSig[i],Back[i],iback); //check to see if denominator is ~1 as it should be
+            //CorrectedPValues[i]=1e-1;     
+           }
+            
+          //we calculated integral from 0 to MeasSig (i.e. the cdf) and the probability we want to use is the cdf_c
+          CorrectedPValues[i]=1-CorrectedPValues[i]; 
+       }      
+       //printf("%f \n",CorrectedPValues[i]/(ROOT::Math::poisson_cdf_c(MeasSig[i],Back[i])+ROOT::Math::poisson_pdf(MeasSig[i],Back[i])/2.)); 
+       //CorrectedPValues[i]=ROOT::Math::poisson_cdf_c(MeasSig[i],Back[i])+ROOT::Math::poisson_pdf(MeasSig[i],Back[i])/2.; 
+        
+        //CorrectedPValues[i]=TOOLS::WeightedP(MeasSig[i],Back[i],aBkgError);
+        //printf("%d: sig=%d back=%f bkgerror=%f corrP=%e\n",i,MeasSig[i],Back[i],aBkgError,CorrectedPValues[i]);
+
+        
         if (Plot) {
-           if (MeasSig[i]<Back[i]) CorrectedIntP*=-1;
-           hSigma->Fill(ProbToSigma(CorrectedIntP));
+           hProb->Fill(CorrectedPValues[i]);  
+           //if (MeasSig[i]<Back[i]) CorrectedPValues[i]*=-1;
+           hSigma->Fill(ROOT::Math::gaussian_quantile_c(CorrectedPValues[i],1));
         }
-     }
-     for (int ib=1;ib<=hProbFlat->GetNbinsX();ib++) hProbFlat->SetBinContent(ib,(float)ent/hProbFlat->GetNbinsX());
-     
-     if (Plot) {
-        cc_cc->cd(1);
-        cc_cc->GetPad(1)->SetLogy();
-        hSig->Draw();
-    
-        hBack->SetLineColor(3);
-        hBack->Draw("SAME"); 
 
-        TLegend * l = new TLegend(0.7,0.8,0.9,0.9);
-        l->AddEntry(hSig,"Measured signal","l");
-        l->AddEntry(hBack,"Background","l");
-        l->Draw();
-
-        cc_cc->cd(2); 
-        hProb->SetMinimum(0);
-        hProb->Draw();
-        hProbFlat->Draw("SAME");
-    
-        cc_cc->cd(3);
      }
      
-     double X[binss],Y[binss],YGOOD[binss],XGOOD[binss];
-     int nintbins=0;
-     for (int ii=0;ii<binss;ii++) {
-         if (!hProb->GetBinContent(ii) && ii!=0 ) continue;
-         XGOOD[nintbins]=hProb->GetBinCenter(ii);
-         YGOOD[nintbins]=ent*(hProb->GetXaxis()->GetBinWidth(1))*(ii);
-         X[nintbins]=hProb->GetBinCenter(ii);
-         Y[nintbins]=hProb->Integral(0,ii);
-         nintbins++;
-      }
+      sort(CorrectedPValues.begin(),CorrectedPValues.end());
+      float Q=TMath::KolmogorovTest(nData,&CorrectedPValues.front(),nData_null,&CorrectedPValues_null.front(),"");
+      if (Q>BestQ) {BestQ=Q;BestProb=anerror;}
+       
   
-      TGraph * gCum = new TGraph(nintbins,X,Y);
-      gCum->SetMarkerStyle(4);
-      gCum->SetMarkerSize(0.2);
-      gCum->SetMinimum(YGOOD[0]);
-      gCum->GetXaxis()->SetTitle("Probability P'");
-      gCum->GetYaxis()->SetTitle("Number of events with P<P'");
-
-
-      TGraph * gCumG = new TGraph(nintbins,XGOOD,YGOOD);
-      gCumG->SetMarkerStyle(2);
-      gCumG->SetMarkerSize(0.2);
-      gCumG->SetMarkerColor(2);
-      
       if (Plot) {
-        gCum->Draw("AP"); 
-        gCumG->Draw("PSAME"); 
-     
-        cc_cc->cd(4);
-        //cc_cc->GetPad(4)->SetLogy();
-        TF1 * fitgaus= new TF1("fit","gaus");
-        fitgaus->SetParameters(1,0,1);
-        fitgaus->FixParameter(1,0);
-        fitgaus->FixParameter(2,1);
-        hSigma->Fit("fit","BQ");
-      }
-      
-      ERROR[nerrors]=anerror;
-      Q[nerrors]=TMath::KolmogorovTest(nintbins,YGOOD,nintbins,Y,"");
-      if (Q[nerrors]>BestQ) {BestQ=Q[nerrors];BestProb=anerror;}
-      gQ = new TGraph(nerrors+1,ERROR,Q);
-      gQ->SetTitle("Kolmogorov-test result");
-      gQ->GetXaxis()->SetTitle("Test systematic error on the bkg estimate");
-      gQ->GetYaxis()->SetTitle("Kolmogorov-test result");
-      
-      if (Plot){ cc_cc->cd(5); gQ->Draw("A*"); }
-      //printf("%f\n",anerror,Q[nerrors]);
-      if (Plot){ cc_cc->Update();  cc_cc->SaveAs("error.gif+");}
+         hProb->Scale(1./nData,"width");
+         c->cd(3); 
+         if (gCum) delete gCum;
+         gCum = new TGraph();
+         for (int ip=1;ip<=hProb->GetNbinsX();ip++) gCum->SetPoint(ip-1,hProb->GetXaxis()->GetBinUpEdge(ip),hProb->Integral(1,ip)/hProb->Integral());
+         gCum->SetMarkerStyle(4);
+         gCum->SetMarkerSize(0.2);
+         
+         gCum->GetXaxis()->SetTitle("Probability P'");
+         gCum->GetYaxis()->SetTitle("Number of events with P<P'");
+         gCum->Draw("AP"); 
+         TLine * ll = new TLine(0,0,1,1);
+         ll->SetLineColor(2); ll->Draw("SAME");
+         
+         c->cd(4);
+         //c->GetPad(4)->SetLogy();
+         hSigma->Fit("fit","BLQ");
+        
+         gQ->SetPoint(nerrors,anerror,Q);
+         //gQ_fit->SetPoint(nerrors,anerror,fitgaus->GetProb());
+         for (int ip=1;ip<=6;ip++) c->GetPad(ip)->Modified() ;
+         
+         if (gProbvsBack) delete gProbvsBack;
+         //gProbvsBack=new TGraph(nData,&CorrectedPValues.front(),&Back.front());
+         //c->cd(6);  gProbvsBack->Draw("A*");
+         
+         c->Update();
+       
+            
+      }  
+    
       nerrors++;
+
    }
 
-   TF1 * fit = new TF1("fit","gaus");
-   gQ->Fit("fit","Q0","",BestProb/2.,BestProb*2);
-   if (Plot) cc_cc->Update(); 
-  // printf("error=%.2f bestQ=%.1f\n",fit->GetParameter(1),BestQ);
- //  getchar();
+
  if (Plot) {
-   delete  hBack;
+    getchar();
+   delete hBack;
    delete hSig;
    delete hSigma;
-
-   delete hProbC;
-   delete  hProbCG;
    
+   delete c;
+   delete fitgaus;
+   delete hProb;  
+   delete gQ; delete gQ_fit;
  }
-   delete hProb;
-   delete  hProbFlat;
-   return fit->GetParameter(1);
+ printf ("e=%f\n", BestProb);
+
+ return BestProb;
 
 }   
 
-double ProbToSigma(double Prob) {
-
-  bool neg=false;
-  short int fac=1;
-  if (Prob<0) {fac=-1; Prob*=-1;}
-  Prob=1-Prob;
-  double sigma=0,aprob=0;
-  double dsigma=0.01;
- 
-  while (1){
-     aprob = 2*ROOT::Math::gaussian_cdf_c(sigma);
-     if (aprob<=Prob) break;
-     //printf("sigma=%f Prob=%f aprob=%f\n",sigma,Prob,aprob);
-     sigma+=dsigma;         
-  };
-  return sigma*fac;
-
-}
-
-
-
-double WeightedP(int nev,double bkg,double dbkg) {
+//(differential) Poisson probability of detecting nev, while expecting bkg+=dbkg
+double WeightedP(int nev, float bkg, float dbkg) { 
 
     double PSum=0,WSum=0;
-    double Start=bkg-3*dbkg;
-    if (Start<0) Start=0;
-    double daback=dbkg/10.;
-    double weight,ap;
-    for (double aback=Start;aback<=bkg+3*dbkg;aback+=daback) {
-       weight = ROOT::Math::gaussian_pdf(aback,dbkg,bkg);
-       ap = ROOT::Math::poisson_pdf(nev,aback);
+    float BkgMin=TMath::Max(0.,bkg-3*dbkg),
+          BkgMax=bkg+3*dbkg,
+          daback=dbkg/10.;
+    
+    
+    for (float aback=BkgMin;aback<=BkgMax;aback+=daback) {
+       double weight = ROOT::Math::gaussian_pdf(aback,dbkg,bkg);
+       double ap     = ROOT::Math::poisson_pdf(nev,aback);
        PSum+=weight*ap*daback;
        WSum+=weight*daback;
     }
-    if (WSum<0.8 || WSum>1) {printf("wsum=%e %d %e %e \n",WSum,nev,bkg,dbkg); exit(1);}
+    if (WSum<0.9 || WSum>1) {printf("%s: wsum=%e %d %e %e \n",__FUNCTION__,WSum,nev,bkg,dbkg); exit(1);}
     return PSum/WSum;
-
 }
-*/
