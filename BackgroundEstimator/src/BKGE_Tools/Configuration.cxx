c11e99f0844dd2ee0d3343cc457c8a4ed01466c5
vlasisva <>
2013-11-13 07:56:16 +0000

    converted exit(1) to runtime errors

--- BackgroundEstimator/src/BKGE_Tools/Configuration.cxx
+++ BackgroundEstimator/src/BKGE_Tools/Configuration.cxx
@@ -10,9 +10,9 @@ void TOOLS::PrintConfig() {
 
 double TOOLS::Get(string name) {
   TDirectory * DataDir = gROOT->GetDirectory("/DataDir");
-  if (!DataDir) {printf("Can't find DataDir!\n"); exit(1);}
+  if (!DataDir) {printf("Can't find DataDir!\n"); throw std::runtime_error("");}
   TNamed * t=((TNamed*)DataDir->FindObject(name.c_str()));
-  if (!t) {printf("%s:Can't find TNamed %s \n",__FUNCTION__,name.c_str()); exit(1);}
+  if (!t) {printf("%s:Can't find TNamed %s \n",__FUNCTION__,name.c_str()); throw std::runtime_error("");}
   return atof(t->GetTitle());
 }
 
@@ -55,9 +55,9 @@ void TOOLS::Set(string name,string vals) {
 
 string TOOLS::GetS(string name) {
   TDirectory * DataDir = gROOT->GetDirectory("/DataDir");
-  if (!DataDir) {printf("Can't find DataDir!\n"); exit(1);}
+  if (!DataDir) {printf("Can't find DataDir!\n"); throw std::runtime_error("");}
   TNamed * t=((TNamed*)DataDir->FindObject(name.c_str()));
-  if (!t) {printf("%s:Can't find TNamed %s \n",__FUNCTION__,name.c_str()); exit(1);}
+  if (!t) {printf("%s:Can't find TNamed %s \n",__FUNCTION__,name.c_str()); throw std::runtime_error("");}
   return t->GetTitle();
 }
 
