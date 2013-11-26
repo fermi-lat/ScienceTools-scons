67de462c78fb68ee50988250be0b8a17b5452507
burnett <>
2013-11-26 04:39:59 +0000

    reimplent counts plot

--- pointlike/python/uw/like2/main.py
+++ pointlike/python/uw/like2/main.py
@@ -154,6 +154,11 @@ class ROI_user(views.LikelihoodViews):
         kwargs.update(galmap=self.roi_dir, annotate=annotation)
         return plotting.sed.stacked_plots(self, **kwargs); 
 
+        
+    @tools.decorate_with(plotting.counts.stacked_plots)
+    def plot_counts(self, **kwargs):
+        return plotting.counts.stacked_plots(self, **kwargs)
+        
 #    @decorate_with(pointlike_plotting.tsmap.plot)
 #    def plot_tsmap(self, source_name=None, **kwargs):
 #        """ create a TS map showing the source localization
@@ -173,11 +178,6 @@ class ROI_user(views.LikelihoodViews):
 #                print 'Failed localization for source %s: %s' % (source.name, e)
 #            tsp = pointlike_plotting.tsmap.plot(loc, **plot_kw)
 #        return tsp
-#        
-#    @decorate_with(pointlike_plotting.counts.stacked_plots)
-#    def plot_counts(self, **kwargs):
-#        return pointlike_plotting.counts.stacked_plots(self, **kwargs)
-#        
 #    @decorate_with(printing.print_summary)
 #    def print_summary(self, **kwargs):
 #        selected_source= self.sources.selected_source
