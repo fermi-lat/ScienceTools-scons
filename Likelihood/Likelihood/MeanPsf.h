12f7583b29f68198a1db61ec56a1c4bc95b86a1a
jchiang <>
2014-05-13 17:47:17 +0000

    cache psf partial integrals for faster integrations in SourceMap::makePointSource

--- Likelihood/Likelihood/MeanPsf.h
+++ Likelihood/Likelihood/MeanPsf.h
@@ -70,7 +70,8 @@ public:
    /// @param energy Energy at which to evaluate the integral (MeV).
    double integral(double angle, double energy) const;
    
-   /// @return The acceptance cone radius for the specified containment fraction (degrees)
+   /// @return The acceptance cone radius for the specified containment
+   ///         fraction (degrees)
    /// @param energy Photon energy (MeV)
    /// @param frac Desired containment fraction (0.68 default)
    double containmentRadius(double energy, double frac=0.68) const;
