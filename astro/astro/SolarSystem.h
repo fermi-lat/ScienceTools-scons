e15a9ff2dbe4a4789ac370973a260985b2cc2fb1
jchiang <>
2015-04-05 04:59:52 +0000

    remove incorrect typedef specification
    Former-commit-id: cc11600e10e159543d6583bcc72adb72e9eb16b0

--- astro/astro/SolarSystem.h
+++ astro/astro/SolarSystem.h
@@ -27,7 +27,8 @@ namespace astro {
         /**
         * index of body that corresponds to the elements in dpleph class
         */
-        typedef enum Body { MERCURY=1,
+//        typedef enum Body { MERCURY=1,
+       enum Body { MERCURY=1,
             VENUS=2, EARTH=3, MARS=4,
             JUPITER=5, SATURN=6, URANUS=7,
             NEPTUNE=8, PLUTO=9, MOON=10, SUN=11};
