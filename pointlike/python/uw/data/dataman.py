0b51378d8b451d19c004370f01f5cf9c823d5c2b
burnett <>
2016-05-17 16:34:24 +0000

    combine THB fixes with EW's event type mods
    Former-commit-id: 6ee7b7351a9cd4fc8ea225c820672cbcec039bb9

--- pointlike/python/uw/data/dataman.py
+++ pointlike/python/uw/data/dataman.py
@@ -397,7 +397,7 @@ class DataSpec(object):
         if gti is None:
             raise DataManException('GTI not found in the binfile %s' % self.binfile)
         self.gti = gti
-        if  (gti.minValue!=self.gti.minValue) or (gti.computeOntime() != self.gti.computeOntime()):
+        if  (gti.minValue!=self.gti.minValue) or abs((gti.computeOntime() - self.gti.computeOntime())>1.0 ):
             print 'File %s Failed GTI check: \n  expect %s \n  found  %s' % (self.binfile, self.gti, gti)
             return self.legacy # ignore if legacy, for now
         
@@ -484,8 +484,9 @@ class DataSpec(object):
         # compare GTI with that found in FT1 or binfile
         #
         gti = skymaps.Gti(self.ltcube)
-        if  (gti.minValue!=self.gti.minValue) or abs(gti.computeOntime() - self.gti.computeOntime())>1:
-            print 'Failed gti check:\n  ltcube: %s \n binfile: %s' % (gti, self.gti)
+        tdiff = gti.computeOntime() - self.gti.computeOntime()
+        if  (gti.minValue()!=self.gti.minValue()) or abs(tdiff)>1:
+            print 'Failed gti check, time difference %.1f:\n  ltcube: %s \n binfile: %s' % (tdiff, gti, self.gti)
             return self.legacy #ignore if legacy, for now
             
         if (not self.quiet): print('Verified ltcube {0}'.format(self.ltcube))
