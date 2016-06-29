Fix some errors in the initial overlap calculation.
Former-commit-id: 4e20069df495e0894aebf83d7866c5e0ae374be9

pointlike/python/uw/irfs/psf.py
@@ -265,6 +265,9 @@ class BandPSF(PSF):
 
     def overlap(self, roi_dir, radius, skydir):
         """Calculate the fractional PSF overlap with a circle."""
+        #NOTE: radius in degrees currently. Seems preferable for it to be radians for
+        #consistency with other PSF code, but would require changes to clients
+        radius = np.radians(radius)
         if hasattr(skydir,'__iter__'):
             scalar = False
             offset = np.asarray([roi_dir.difference(sd) for sd in skydir])
@@ -273,8 +276,9 @@ class BandPSF(PSF):
             offset = np.asarray([roi_dir.difference(skydir)])
         ret = np.zeros(offset.shape)
         interior = offset<=radius
+        lim = np.arcsin(radius/offset)
         ret[interior] = np.array([integrate.quad(self._interior_integrand(o,radius),0,np.pi)[0]/np.pi for o in offset[interior]])
-        ret[~interior] = np.array([integrate.quad(self._exterior_integrand(o,radius),0,np.pi)[0]/np.pi for o in offset[~interior]])
+        ret[~interior] = np.array([integrate.quad(self._exterior_integrand(o,radius),0,np.arcsin(radius/l))[0]/np.pi for o,l in zip(offset[~interior],lim[~interior])])
         if scalar:
             return ret.item()
         else:
@@ -291,7 +295,6 @@ class BandPSF(PSF):
         def integrand(theta):
             ctheta = np.cos(theta)
             x = (radius**2+offset**2*(ctheta**2-1))**.5
-            rmax =  - ctheta*offset
-            return self.integral(radius*ctheta+x,radius*ctheta-x)
+            return self.integral(offset*ctheta+x,offset*ctheta-x)
         return integrand
 