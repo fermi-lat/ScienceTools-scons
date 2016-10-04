6e56184ff176523fedf2e77cfe6766a5a8a8a95f
jchiang <>
2016-10-04 20:29:23 +0000

    use try/except block to isolate gsllibs dependency
    Former-commit-id: 51dced5dee72d29dd37d3038bb8ccf4965cde17b

--- st_facilities/st_facilitiesLib.py
+++ st_facilities/st_facilitiesLib.py
@@ -8,8 +8,10 @@ def generate(env, **kw):
     env.Tool('addLibrary', library = env['cfitsioLibs'])
     env.Tool('addLibrary', library = env['f2cLibs'])
     env.Tool('addLibrary', library = env['cppunitLibs'])
-    if env.get['CONTAINERNAME', '') == 'ScienceTools':
+    try:
         env.Tool('addLibrary', library = env['gsllibs'])
+    except:
+        pass
 
 def exists(env):
     return 1
